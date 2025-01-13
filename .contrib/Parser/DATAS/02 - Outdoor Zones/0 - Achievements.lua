8e89be298bf55541009adb8a71bc66ee905aca99
Chun-Chi Hung <b10102229@gmail.com>
2025-01-14 00:37:49 +0800

    [DB] Try to fix syntax.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/0 - Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/0 - Achievements.lua
@@ -84,7 +84,9 @@ root(ROOTS.Zones, {
 		}),
 		ach(31, {		-- A Simple Re-Quest
 			["timeline"] = { ADDED_3_0_2 },
-			crit(72),
+			["groups"] = {
+				crit(72),
+			},
 		}),
 		applyclassicphase(PHASE_ONE, achWithReps(948, {	-- Ambassador of the Alliance
 		FACTION_DARNASSUS, FACTION_GNOMEREGAN, FACTION_IRONFORGE, FACTION_STORMWIND,		-- Exalted Darnassus, Gnomeregan, Ironforge, Stormwind City
