343e1d0277a1229b958a77cef48cb42806129537
ImUnicke <ImUnicke+github@gmail.com>
2025-01-16 00:26:30 -0700

    Another Oribos HQT moved

--- .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/1 - Oribos/The Crucible.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/13 Shadowlands/1 - Oribos/The Crucible.lua
@@ -6,24 +6,31 @@ root(ROOTS.Zones, m(SHADOWLANDS, bubbleDown({ ["timeline"] = { ADDED_9_0_2_LAUNC
 			["sourceQuest"] = 63857,	-- Voices of the Eternal
 			["g"] = {
 				n(HIDDEN_QUESTS, {
+					hqt(65614, {	-- Stay awhile and listen with Arbiter Pelagos (spellID 366958)
+						["timeline"] = {ADDED_9_2_0},
+						["name"] = "Stay awhile and listen: Arbiter Pelagos",
+						["sourceQuest"] = 63857,	-- Voices of the Eternal
+						["provider"] = { "n", 183830 },	-- Arbiter Pelagos
+						["coord"] = { 68.0, 48.6, 1673 },	-- The Crucible
+					}),
 					hqt(65612, {	-- Stay awhile and listen with Baine (spellID 366952)
 						["timeline"] = {ADDED_9_2_0},
 						["name"] = "Stay awhile and listen: Baine Bloodhoof",
-						-- ["sourceQuests"] = { },	-- unknown
+						["sourceQuest"] = 63857,	-- Voices of the Eternal
 						["provider"] = { "n", 184098 },	-- Baine Bloodhoof
 						["coord"] = { 42.5, 34.0, 1673 },	-- The Crucible
 					}),
 					hqt(65609, {	-- Stay awhile and listen with Lor'themar Theron (spellID 366932)
 						["timeline"] = {ADDED_9_2_0},
 						["name"] = "Stay awhile and listen: Lor'themar Theron",
-						-- ["sourceQuests"] = { },	-- unknown
+						["sourceQuest"] = 63857,	-- Voices of the Eternal
 						["provider"] = { "n", 184100 },	-- Lor'themar Theron
 						["coord"] = { 44.2, 33.2, 1673 },	-- The Crucible
 					}),
 					hqt(65607, {	-- Stay awhile and listen with King Greymane (spellID 366925)
 						["timeline"] = {ADDED_9_2_0},
 						["name"] = "Stay awhile and listen: Genn Greymane",
-						-- ["sourceQuests"] = { },	-- unknown
+						["sourceQuest"] = 63857,	-- Voices of the Eternal
 						["provider"] = { "n", 184088 },	-- Genn Greymane
 						["coord"] = { 54.2, 28.2, 1673 },	-- The Crucible
 					}),
