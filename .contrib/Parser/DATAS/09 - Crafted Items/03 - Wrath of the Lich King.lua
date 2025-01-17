c21dbf9e7054f393a33d4fa9aada129bd69b2065
DannyDonkey <145834650+DannyDonkey@users.noreply.github.com>
2025-01-17 05:17:23 +0100

    Another ritual of the new moon.

--- .contrib/Parser/DATAS/09 - Crafted Items/03 - Wrath of the Lich King.lua
+++ .contrib/Parser/DATAS/09 - Crafted Items/03 - Wrath of the Lich King.lua
@@ -774,12 +774,14 @@ root(ROOTS.Craftables, expansion(EXPANSION.WRATH, applyclassicphase(WRATH_PHASE_
 			i(38322),	-- Iron-Bound Tome
 			i(45854, {	-- Rituals of the New Moon
 				["description"] = "This version of the trinket is a placeholder for the four available versions of it, and crafting it gives you a random one of the four. The four available versions of the trinket have the same stats, but offer a different coloured giant wholf.",
-				["timeline"] = { CREATED_3_1_0 },
+				["timeline"] = { ADDED_3_1_0 },
+				["groups"] = {
+					i(45850),	-- Rituals of the New Moon (red wolf)
+					i(45851),	-- Rituals of the New Moon (white wolf)
+					i(45852),	-- Rituals of the New Moon (grey wolf)
+					i(45853),	-- Rituals of the New Moon (black wolf)
+				},
 			}),
-			i(45850),	-- Rituals of the New Moon
-			i(45851),	-- Rituals of the New Moon
-			i(45852),	-- Rituals of the New Moon
-			i(45853),	-- Rituals of the New Moon
 			i(45849),	-- Twilight Tome
 		}),
 	}),
