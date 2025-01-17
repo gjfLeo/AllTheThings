6f820e3e9813e65cc8440db7cde4b3378335cd02
Chun-Chi Hung <b10102229@gmail.com>
2025-01-18 02:37:42 +0800

    [DB] add SoD dungeon: Karazhan Crypts.

---
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Karazhan Crypts.lua
@@ -0,0 +1,10 @@
+-- #if SEASON_OF_DISCOVERY
+-----------------------------------------------------
+--   D U N G E O N S  &  R A I D S  M O D U L E    --
+-----------------------------------------------------
+root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
+	applyclassicphase(SOD_PHASE_SEVEN, inst(2875, bubbleDownSelf({["timeline"] = { "added 1.15.6" }}, {	-- Karazhan Crypts
+		["zone-text-areaID"] = 16074,	-- Karazhan Crypts
+	}))),
+}));
+-- #endif
