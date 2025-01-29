5133c66875e64c73d1060941f056f90e518efaec
Chun-Chi Hung <b10102229@gmail.com>
2025-01-29 17:50:42 +0800

    [DB] SoD: Update lvl/coord of Karazhan Crypts.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Karazhan Crypts.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Karazhan Crypts.lua
@@ -6,8 +6,8 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
 	applyclassicphase(SOD_PHASE_SEVEN, inst(2875, bubbleDownSelf({["timeline"] = { "added 1.15.6" }}, {	-- Karazhan Crypts
 		["zone-text-areaID"] = 16074,	-- Karazhan Crypts
 		["description"] = "[TBC] This is a new dungeon added in SoD Phase 7. \nThere is no detailed information yet. \nIt will be added after the wowhead guide is updated. \nIf you have any information, please leave a message under \"Karazhan Crypts Dungeon Overview - Season of Discovery\" on wowhead.",
-		["lvl"] = 60,
-		["coord"] = { 50.0, 50.0, DEADWIND_PASS },
+		["lvl"] = 58,
+		["coord"] = { 39.8, 73.6, DEADWIND_PASS },
 	}))),
 }));
 -- #endif
