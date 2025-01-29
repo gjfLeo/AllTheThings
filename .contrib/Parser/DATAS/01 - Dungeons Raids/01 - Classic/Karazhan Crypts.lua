a5fabd40f9cc88ebbc20938bde5a6e489ccbc4b2
Chun-Chi Hung <b10102229@gmail.com>
2025-01-29 23:28:40 +0800

    [DB] SoD: Add qg of quests in Karazhan Crypts.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Karazhan Crypts.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Karazhan Crypts.lua
@@ -16,29 +16,35 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
 					["lvl"] = 60,
 				}),	
 				q(86965, {	-- No Ordinary Shadows
+					["qg"] = 237820,	-- Deceased Adventurer
 					["coord"] = { 39.0, 74.0, DEADWIND_PASS },
 					["sourceQuest"] = 86964,	-- For Gold and Glory!
 					["lvl"] = 60,
 				}),
 				q(86966, {	-- Seeking Survivors
+					["qg"] = 218920,	-- Dalaran Agent <Violet Eye>
 					["coord"] = { 52.0, 34.0, DEADWIND_PASS },
 					["sourceQuest"] = 86965,	-- No Ordinary Shadows
 					["lvl"] = 60,
 				}),
 				q(86967, {	-- To the Rescue
+					["qg"] = 237819,	-- Injured Adventurer
 					["coord"] = { 59.2, 73.4, DEADWIND_PASS },
 					["sourceQuest"] = 86966,	-- Seeking Survivors
 					["lvl"] = 60,
 				}),
 				q(86968, {	-- Are You Afraid of the Dark?
+					["qg"] = 237818,	-- Harrison Jones
 					["sourceQuest"] = 86967,	-- To the Rescue
 					["lvl"] = 60,
 				}),
 				q(86969, {	-- The Hypothesis
+					["qg"] = 218920,	-- Dalaran Agent <Violet Eye>
 					["sourceQuest"] = 86968,	-- Are You Afraid of the Dark?
 					["lvl"] = 60,
 				}),
 				q(86970, {	-- Testing Our Hypothesis
+					["qg"] = 218920,	-- Dalaran Agent <Violet Eye>
 					["sourceQuest"] = 86969,	-- The Hypothesis
 					["lvl"] = 60,
 				}),
