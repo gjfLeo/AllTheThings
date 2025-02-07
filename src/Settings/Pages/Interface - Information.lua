3014da3aef54ddc8edc07f7e3d2a6024be3acfa8
ImUnicke <ImUnicke+github@gmail.com>
2025-02-07 11:30:26 -0700

    [Tooltip] 'Maps' information type now uses reference 'coords' if no 'maps' defined (feedback welcome on if this should be reduced to certain Types of Things, but it's pretty minimal and helpful to guide on where to travel at a glance) [Tooltip] 'Maps' information type no longer excludes showing the current map name when applicable

--- src/Settings/Pages/Interface - Information.lua
+++ src/Settings/Pages/Interface - Information.lua
@@ -593,18 +593,24 @@ local InformationTypes = {
 	CreateInformationType("maps", { text = L.MAPS, priority = 2.6,
 		Process = function(t, reference, tooltipInfo)
 			local maps = reference.maps;
+			if not maps or #maps == 0 then
+				local coords = reference.coords
+				if coords and #coords > 0 then
+					maps = {}
+					for _,coord in ipairs(coords) do
+						maps[#maps + 1] = coord[3]
+					end
+				end
+			end
 			if maps and #maps > 0 then
-				local currentMapID = app.CurrentMapID;
 				local mapNames,uniques,name = {},{},nil;
 				local rootMapID = reference.mapID;
 				if rootMapID then uniques[app.GetMapName(rootMapID) or rootMapID] = true; end
 				for i,mapID in ipairs(maps) do
-					if mapID ~= currentMapID then
-						name = app.GetMapName(mapID);
-						if name and not uniques[name] then
-							uniques[name] = true;
-							tinsert(mapNames, name);
-						end
+					name = app.GetMapName(mapID);
+					if name and not uniques[name] then
+						uniques[name] = true;
+						tinsert(mapNames, name);
 					end
 				end
 				if #mapNames > 0 then
