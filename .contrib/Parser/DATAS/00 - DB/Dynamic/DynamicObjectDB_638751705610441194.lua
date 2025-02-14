7386f4439eff977206b5744bd478d420c9a27dec
Myrhial <8618272+ninaarens@users.noreply.github.com>
2025-02-14 23:56:51 +0100

    Harvest sources on PTR

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638751705610441194.lua
@@ -0,0 +1,61 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[478679] = {
+		readable = "Salvage Part",
+		model = 203234,
+		text = {
+			en = "Salvage Part",
+			es = "Salvage Part",
+			de = "Salvage Part",
+			fr = "Salvage Part",
+			it = "Salvage Part",
+			pt = "Salvage Part",
+			ru = "Salvage Part",
+			ko = "Salvage Part",
+			cn = "Salvage Part",
+		},
+	},
+	[500759] = {
+		readable = "Uncovered Strongbox",
+		text = {
+			en = "Uncovered Strongbox",
+			es = "Uncovered Strongbox",
+			de = "Uncovered Strongbox",
+			fr = "Uncovered Strongbox",
+			it = "Uncovered Strongbox",
+			pt = "Uncovered Strongbox",
+			ru = "Uncovered Strongbox",
+			ko = "Uncovered Strongbox",
+			cn = "Uncovered Strongbox",
+		},
+	},
+	[503450] = {
+		readable = "Chunk of Charged Glass",
+		text = {
+			en = "Chunk of Charged Glass",
+			es = "Chunk of Charged Glass",
+			de = "Chunk of Charged Glass",
+			fr = "Chunk of Charged Glass",
+			it = "Chunk of Charged Glass",
+			pt = "Chunk of Charged Glass",
+			ru = "Chunk of Charged Glass",
+			ko = "Chunk of Charged Glass",
+			cn = "Chunk of Charged Glass",
+		},
+	},
+	[503870] = {
+		readable = "Nemesis Strongbox",
+		model = 2907352,
+		text = {
+			en = "Nemesis Strongbox",
+			es = "Nemesis Strongbox",
+			de = "Nemesis Strongbox",
+			fr = "Nemesis Strongbox",
+			it = "Nemesis Strongbox",
+			pt = "Nemesis Strongbox",
+			ru = "Nemesis Strongbox",
+			ko = "Nemesis Strongbox",
+			cn = "Nemesis Strongbox",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
