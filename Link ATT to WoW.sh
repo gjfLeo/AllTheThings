bb40510dab23f423bda4b79a065058ad133f69fe
Chun-Chi Hung <b10102229@gmail.com>
2025-03-02 04:04:32 +0800

    [Misc.] change indent space to 4.

--- Link ATT to WoW.sh
+++ Link ATT to WoW.sh
@@ -2,94 +2,94 @@
 set -e
 
 link_wowfolder() {
-  if [[ -d "$1" ]]; then
-    link_expansion "$1/_classic_"
-    link_expansion "$1/_classic_era_"
-    link_expansion "$1/_classic_beta_"
-    link_expansion "$1/_classic_ptr_"
-    link_expansion "$1/_classic_era_ptr_"
-    link_expansion "$1/_retail_"
-    link_beta "$1/_beta_"
-    link_ptr "$1/_ptr_"
-    link_ptr "$1/_xptr_"
-  fi
+    if [[ -d "$1" ]]; then
+        link_expansion "$1/_classic_"
+        link_expansion "$1/_classic_era_"
+        link_expansion "$1/_classic_beta_"
+        link_expansion "$1/_classic_ptr_"
+        link_expansion "$1/_classic_era_ptr_"
+        link_expansion "$1/_retail_"
+        link_beta "$1/_beta_"
+        link_ptr "$1/_ptr_"
+        link_ptr "$1/_xptr_"
+    fi
 }
 
 link_expansion() {
-  if [[ -d "$1" ]]; then
-    echo "Linking Expansion $1"
-    if [[ -d "$1/Interface/AddOns/AllTheThings" ]]; then
-      echo "Removing ATT in $1"
-      rm -rf "$1/Interface/AddOns/AllTheThings"
+    if [[ -d "$1" ]]; then
+        echo "Linking Expansion $1"
+        if [[ -d "$1/Interface/AddOns/AllTheThings" ]]; then
+            echo "Removing ATT in $1"
+            rm -rf "$1/Interface/AddOns/AllTheThings"
+        fi
+        if [[ ! -d "$1/Interface/AddOns/AllTheThings" ]]; then
+            echo "Adding ATT in $1"
+            mkdir -p "$1/Interface/AddOns"
+            rsync -a  --link-dest="$PWD/" --exclude='.*' "$PWD/" "$1/Interface/AddOns/AllTheThings"
+        fi
     fi
-    if [[ ! -d "$1/Interface/AddOns/AllTheThings" ]]; then
-      echo "Adding ATT in $1"
-      mkdir -p "$1/Interface/AddOns"
-      rsync -a  --link-dest="$PWD/" --exclude='.*' "$PWD/" "$1/Interface/AddOns/AllTheThings"
-    fi
-  fi
 }
 
 link_beta() {
-  if [[ -d "$1" ]]; then
-    echo "Linking BETA $1"
-    link_beta_files "$1/Interface/AddOns/AllTheThings"
-  fi
+    if [[ -d "$1" ]]; then
+        echo "Linking BETA $1"
+        link_beta_files "$1/Interface/AddOns/AllTheThings"
+    fi
 }
 
 link_beta_files() {
-  echo "Linking BETA Files $1"
-  if [[ -d "$1" ]]; then
-    rm -rf "$1"
-  fi
-  if [[ ! -d "$1" ]]; then
-    mkdir -p "$1/db/Retail"
-    rsync -a --link-dest="$PWD/db/Retail/.beta/Categories.lua" "$PWD/db/Retail/.beta/Categories.lua" "$1/db/Retail/Categories.lua"
-    rsync -a --link-dest="$PWD/db/Retail/.beta/LocalizationDB.lua" "$PWD/db/Retail/.beta/LocalizationDB.lua" "$1/db/Retail/LocalizationDB.lua"
-    rsync -a --link-dest="$PWD/db/Retail/.beta/ReferenceDB.lua" "$PWD/db/Retail/.beta/ReferenceDB.lua" "$1/db/Retail/ReferenceDB.lua"
+    echo "Linking BETA Files $1"
+    if [[ -d "$1" ]]; then
+        rm -rf "$1"
+    fi
+    if [[ ! -d "$1" ]]; then
+        mkdir -p "$1/db/Retail"
+        rsync -a --link-dest="$PWD/db/Retail/.beta/Categories.lua" "$PWD/db/Retail/.beta/Categories.lua" "$1/db/Retail/Categories.lua"
+        rsync -a --link-dest="$PWD/db/Retail/.beta/LocalizationDB.lua" "$PWD/db/Retail/.beta/LocalizationDB.lua" "$1/db/Retail/LocalizationDB.lua"
+        rsync -a --link-dest="$PWD/db/Retail/.beta/ReferenceDB.lua" "$PWD/db/Retail/.beta/ReferenceDB.lua" "$1/db/Retail/ReferenceDB.lua"
 
-    rsync -a --link-dest="$PWD/assets/" "$PWD/assets/" "$1/assets"
-    rsync -a --link-dest="$PWD/lib/" "$PWD/lib/" "$1/lib"
-    rsync -a --link-dest="$PWD/locales/" "$PWD/locales/" "$1/locales"
-    rsync -a --link-dest="$PWD/src/" "$PWD/src/" "$1/src"
+        rsync -a --link-dest="$PWD/assets/" "$PWD/assets/" "$1/assets"
+        rsync -a --link-dest="$PWD/lib/" "$PWD/lib/" "$1/lib"
+        rsync -a --link-dest="$PWD/locales/" "$PWD/locales/" "$1/locales"
+        rsync -a --link-dest="$PWD/src/" "$PWD/src/" "$1/src"
 
-    rsync -a --link-dest="$PWD/AllTheThings.lua" "$PWD/AllTheThings.lua" "$1/AllTheThings.lua"
-    rsync -a --link-dest="$PWD/AllTheThings.toc" "$PWD/AllTheThings.toc" "$1/AllTheThings.toc"
-    rsync -a --link-dest="$PWD/Bindings.xml" "$PWD/Bindings.xml" "$1/Bindings.xml"
-  fi
+        rsync -a --link-dest="$PWD/AllTheThings.lua" "$PWD/AllTheThings.lua" "$1/AllTheThings.lua"
+        rsync -a --link-dest="$PWD/AllTheThings.toc" "$PWD/AllTheThings.toc" "$1/AllTheThings.toc"
+        rsync -a --link-dest="$PWD/Bindings.xml" "$PWD/Bindings.xml" "$1/Bindings.xml"
+    fi
 }
 
 link_ptr() {
-  if [[ -d "$1" ]]; then
-    echo "Linking PTR $1"
-    link_ptr_files "$1/Interface/AddOns/AllTheThings"
-  fi
+    if [[ -d "$1" ]]; then
+        echo "Linking PTR $1"
+        link_ptr_files "$1/Interface/AddOns/AllTheThings"
+    fi
 }
 
 link_ptr_files() {
-  echo "Linking PTR Files $1"
-  if [[ -d "$1" ]]; then
-    rm -rf "$1"
-  fi
-  if [[ ! -d "$1" ]]; then
-    mkdir -p "$1/db/Retail"
-    rsync -a --link-dest="$PWD/db/Retail/.ptr/Categories.lua" "$PWD/db/Retail/.ptr/Categories.lua" "$1/db/Retail/Categories.lua"
-    rsync -a --link-dest="$PWD/db/Retail/.ptr/LocalizationDB.lua" "$PWD/db/Retail/.ptr/LocalizationDB.lua" "$1/db/Retail/LocalizationDB.lua"
-    rsync -a --link-dest="$PWD/db/Retail/.ptr/ReferenceDB.lua" "$PWD/db/Retail/.ptr/ReferenceDB.lua" "$1/db/Retail/ReferenceDB.lua"
+    echo "Linking PTR Files $1"
+    if [[ -d "$1" ]]; then
+        rm -rf "$1"
+    fi
+    if [[ ! -d "$1" ]]; then
+        mkdir -p "$1/db/Retail"
+        rsync -a --link-dest="$PWD/db/Retail/.ptr/Categories.lua" "$PWD/db/Retail/.ptr/Categories.lua" "$1/db/Retail/Categories.lua"
+        rsync -a --link-dest="$PWD/db/Retail/.ptr/LocalizationDB.lua" "$PWD/db/Retail/.ptr/LocalizationDB.lua" "$1/db/Retail/LocalizationDB.lua"
+        rsync -a --link-dest="$PWD/db/Retail/.ptr/ReferenceDB.lua" "$PWD/db/Retail/.ptr/ReferenceDB.lua" "$1/db/Retail/ReferenceDB.lua"
 
-    rsync -a --link-dest="$PWD/assets/" "$PWD/assets/" "$1/assets"
-    rsync -a --link-dest="$PWD/lib/" "$PWD/lib/" "$1/lib"
-    rsync -a --link-dest="$PWD/locales/" "$PWD/locales/" "$1/locales"
-    rsync -a --link-dest="$PWD/src/" "$PWD/src/" "$1/src"
+        rsync -a --link-dest="$PWD/assets/" "$PWD/assets/" "$1/assets"
+        rsync -a --link-dest="$PWD/lib/" "$PWD/lib/" "$1/lib"
+        rsync -a --link-dest="$PWD/locales/" "$PWD/locales/" "$1/locales"
+        rsync -a --link-dest="$PWD/src/" "$PWD/src/" "$1/src"
 
-    rsync -a --link-dest="$PWD/AllTheThings.lua" "$PWD/AllTheThings.lua" "$1/AllTheThings.lua"
-    rsync -a --link-dest="$PWD/AllTheThings_Mainline.toc" "$PWD/AllTheThings_Mainline.toc" "$1/AllTheThings_Mainline.toc"
-    rsync -a --link-dest="$PWD/Bindings.xml" "$PWD/Bindings.xml" "$1/Bindings.xml"
-  fi
+        rsync -a --link-dest="$PWD/AllTheThings.lua" "$PWD/AllTheThings.lua" "$1/AllTheThings.lua"
+        rsync -a --link-dest="$PWD/AllTheThings_Mainline.toc" "$PWD/AllTheThings_Mainline.toc" "$1/AllTheThings_Mainline.toc"
+        rsync -a --link-dest="$PWD/Bindings.xml" "$PWD/Bindings.xml" "$1/Bindings.xml"
+    fi
 }
 
 report_taskcomplete() {
-  echo "Task Complete!"
+    echo "Task Complete!"
 }
 
 # run the script
