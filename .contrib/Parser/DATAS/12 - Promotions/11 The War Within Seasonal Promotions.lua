104e7df5b62fa9c664681850c49d3cad24e8830c
Goldenshacal <goldenshacal@gmail.com>
2025-03-08 12:33:46 +0100

    new ssn new seasonal toys

--- .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
@@ -251,6 +251,14 @@ root(ROOTS.Promotions, {
 				["u"] = REAL_MONEY,
 			}),
 			-- Season 2
+			i(232306, {	-- Prized Champion's Prestigious Banner (TOY!)(PVP)
+			--	["description"] = "Arena World Championship: TWW Season 2\n\nSign up on Raider.io for any of the 4 Cups, available until October 13th 2024, and play in at least two game series (best of 5)\n\nWinning not required, for more details & requirements checkout: Raider.io/tournaments/AWC",
+				["timeline"] = { ADDED_11_1_5, REMOVED_11_2_0 },
+			}),
+			i(232302, {	-- Prized Banner of the Algari (TOY!)
+			--	["description"] = "Mythic Dungeon International: TWW Season 1\n\nThe Mythic Dungeon International (MDI) returns with its global competitions, pitting the best Mythic Dungeon teams in a head-to-head race to the finish line.\n\nAll registered teams that complete under time the two dungeons within the Proving Grounds will receive the exclusive Prized Banner of the Algari to use in-game!\nSign-ups close 14 October 2024 1PM PDT and The Time Trials are on 16 October 1PM PDT - 21 October (US) 1PM PDT.",
+				["timeline"] = { ADDED_11_1_5, REMOVED_11_2_0 },
+			}),
 		--	i(500001, {    -- The PVE/PVP Banner temporary item -- TEMPLATE
 		--		["sourceID"] = 500001,
 		--		["description"] = "Mythic Dungeon International: TWW Season 1\n\nThe Mythic Dungeon International (MDI) returns with its global competitions, pitting the best Mythic Dungeon teams in a head-to-head race to the finish line.\n\nAll registered teams that complete under time the two dungeons within the Proving Grounds will receive the exclusive Tempered Banner of the Algari to use in-game!\nSign-ups close 14 October 2024 1PM PDT and The Time Trials are on 16 October 1PM PDT - 21 October (US) 1PM PDT.",
