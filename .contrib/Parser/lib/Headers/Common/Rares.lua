d3746339490751657f2c6d46d19aacc05ac64c69
Crieve <dylanfortune@live.com>
2025-03-16 10:14:53 -0700

    Classic: Fixed the "Rares" header.

--- .contrib/Parser/lib/Headers/Common/Rares.lua
+++ .contrib/Parser/lib/Headers/Common/Rares.lua
@@ -4,6 +4,12 @@ RARES = createHeader({
 	export = true,
 	icon = [[~_.asset("Interface_Rare")]],
 	text = {
+		-- #if AFTER LEGION
 		en = [[~MAP_LEGEND_RARE]],
+		-- #else
+		en = [[~BATTLE_PET_BREED_QUALITY4]],
+		ru = "Редкие",
+		cn = "稀有",
+		-- #endif
 	},
 });
