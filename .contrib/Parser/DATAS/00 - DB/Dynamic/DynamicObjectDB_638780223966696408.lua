364570789cfdd938b602ab0dc06ef5392e2aa930
gbrage <gbrage@kth.se>
2025-03-20 00:07:30 +0100

    Reparse Retail and PTR

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638780223966696408.lua
@@ -0,0 +1,33 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[499960] = {
+		readable = "Corrupted Chest",
+		model = 3048901,
+		text = {
+			en = "Corrupted Chest",
+			es = "Corrupted Chest",
+			de = "Corrupted Chest",
+			fr = "Corrupted Chest",
+			it = "Corrupted Chest",
+			pt = "Corrupted Chest",
+			ru = "Corrupted Chest",
+			ko = "Corrupted Chest",
+			cn = "Corrupted Chest",
+		},
+	},
+	[505680] = {
+		readable = "Animatronic Gear",
+		model = 2067157,
+		text = {
+			en = "Animatronic Gear",
+			es = "Animatronic Gear",
+			de = "Animatronic Gear",
+			fr = "Animatronic Gear",
+			it = "Animatronic Gear",
+			pt = "Animatronic Gear",
+			ru = "Animatronic Gear",
+			ko = "Animatronic Gear",
+			cn = "Animatronic Gear",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
