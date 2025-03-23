a8cad6ff32ae7f88b59efb9c6bcf133f7231a1bf
Chun-Chi Hung <b10102229@gmail.com>
2025-03-23 18:44:09 +0800

    [Locale] Update zhCN/zhTW: Battlefield: Barrens.

--- .contrib/Parser/DATAS/06 - Expansion Features/05 - Mists of Pandaria/Battlefield.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/05 - Mists of Pandaria/Battlefield.lua
@@ -6,15 +6,16 @@ BATTLEFIELD_BARRENS = createHeader({
 	icon = 252178,
 	text = {
 		en = "Battlefield: Barrens",
-		es = "Campo de batalla: Los Baldíos",
 		de = "Schlachtfeld: Brachland",
+		es = "Campo de batalla: Los Baldíos",
+		-- mx = "Campo de batalla: Los Baldíos",
 		fr = "Champ de bataille : les Tarides",
 		it = "Campo di battaglia: le Savane",
+		ko = "전쟁터: 불모의 땅",
 		pt = "Campo de Batalha: Sertões",
 		ru = "Поле битвы: Степи",
-		ko = "전쟁터: 불모의 땅",
-		cn = "战场：贫瘠之地",
-		tw = "戰爭前線 - 貧瘠之地",
+		cn = "战线：贫瘠之地",
+		tw = "戰爭前線：貧瘠之地",
 	},
 });
 local RADICAL_MOJO = 97849;
