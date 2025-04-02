45ee3d1c5177752afe059ae7dea37e08338c1bcb
ImUnicke <ImUnicke+github@gmail.com>
2025-04-02 12:19:41 -0600

    [DB] Removed Cost data for Handcrank and Steamboil since they're included in ReagentDB

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Undermine/Zone Drops.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Undermine/Zone Drops.lua
@@ -8,22 +8,24 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_1_0 } }, 
 			i(236668),	-- C.H.E.T.T. Card
 			-- TODO: idk where to place it better here or in rewards?
 			i(232984, {	-- Handcrank (MM!)
-				["cost"] = {
-					{ "i", 234415, 1 },	-- Handcrank Casing
-					{ "i", 234386, 1 },	-- Handcrank Fuel Injector
-					{ "i", 234381, 1 },	-- Handcrank Fuel Tank
-					{ "i", 234417, 1 },	-- Handcrank Gears
-					{ "i", 234420, 1 },	-- Handcrank Mounting System
-				},
+				-- Included in ReagentDB
+				-- ["cost"] = {
+				-- 	{ "i", 234415, 1 },	-- Handcrank Casing
+				-- 	{ "i", 234386, 1 },	-- Handcrank Fuel Injector
+				-- 	{ "i", 234381, 1 },	-- Handcrank Fuel Tank
+				-- 	{ "i", 234417, 1 },	-- Handcrank Gears
+				-- 	{ "i", 234420, 1 },	-- Handcrank Mounting System
+				-- },
 			}),
 			i(232983, {	-- Steamboil (MM!)
-				["cost"] = {
-					{ "i", 234416, 1 },	-- Steamboil Casing
-					{ "i", 234387, 1 },	-- Steamboil Fuel Injector
-					{ "i", 234380, 1 },	-- Steamboil Fuel Tank
-					{ "i", 234418, 1 },	-- Steamboil Gears
-					{ "i", 234419, 1 },	-- Steamboil Mounting System
-				},
+				-- Included in ReagentDB
+				-- ["cost"] = {
+				-- 	{ "i", 234416, 1 },	-- Steamboil Casing
+				-- 	{ "i", 234387, 1 },	-- Steamboil Fuel Injector
+				-- 	{ "i", 234380, 1 },	-- Steamboil Fuel Tank
+				-- 	{ "i", 234418, 1 },	-- Steamboil Gears
+				-- 	{ "i", 234419, 1 },	-- Steamboil Mounting System
+				-- },
 			}),
 		}),
 	}),
