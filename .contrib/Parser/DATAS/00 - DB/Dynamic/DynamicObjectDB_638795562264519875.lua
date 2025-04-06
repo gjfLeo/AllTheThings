f1bf62c75ffe4f8d3947f9b40fdfe6282a21d288
Crieve <dylanfortune@live.com>
2025-04-06 10:10:47 -0700

    Added missing object data.

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638795562264519875.lua
@@ -0,0 +1,18 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[410779] = {
+		readable = "Offering Box",
+		model = 198238,
+		text = {
+			en = "Offering Box",
+			es = "Caja de ofrendas",
+			de = "Opferkasten",
+			fr = "Boîte d’offrandes",
+			it = "[Offering Box]",
+			pt = "Caixa de Oferenda",
+			ru = "Ларец для подношений",
+			ko = "공물 상자",
+			cn = "[Offering Box]",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
