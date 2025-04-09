0eaf129fb860d1af81a2f43f3e1c7274a76c6410
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2025-04-08 22:43:34 -0700

    Grrrr - Parse after new objects plus contrib stuffs

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638797577384153278.lua
@@ -0,0 +1,33 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[446580] = {
+		readable = "Radiant Fuel Cache",
+		model = 4860703,
+		text = {
+			en = "Radiant Fuel Cache",
+			es = "Alijo de combustible radiante",
+			de = "Strahlende Treibstofftruhe",
+			fr = "Cache de carburant radieux",
+			it = "Cassa di Carburante Radioso",
+			pt = "Depósito de Combustível Radiante",
+			ru = "Тайник с сияющими кристаллами",
+			ko = "빛나는 연료 보관함",
+			cn = "光耀燃料宝箱",
+		},
+	},
+	[451222] = {
+		readable = "Radiant Fuel Cache",
+		model = 4860703,
+		text = {
+			en = "Radiant Fuel Cache",
+			es = "Alijo de combustible radiante",
+			de = "Strahlende Treibstofftruhe",
+			fr = "Cache de carburant radieux",
+			it = "Cassa di Carburante Radioso",
+			pt = "Depósito de Combustível Radiante",
+			ru = "Тайник с сияющими кристаллами",
+			ko = "빛나는 연료 보관함",
+			cn = "光耀燃料宝箱",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
