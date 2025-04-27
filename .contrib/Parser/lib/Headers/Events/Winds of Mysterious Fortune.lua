f961a267d516162b4d94095b8de419ed09234bf4
Chun-Chi Hung <b10102229@gmail.com>
2025-04-28 02:36:27 +0800

    [Locale] Update zhTW: Winds of Mysterious Fortune.

--- .contrib/Parser/lib/Headers/Events/Winds of Mysterious Fortune.lua
+++ .contrib/Parser/lib/Headers/Events/Winds of Mysterious Fortune.lua
@@ -10,9 +10,11 @@ WINDS_OF_MYSTERIOUS_FORTUNE_HEADER = createHeader({
 	text = {
 		en = "Winds of Mysterious Fortune",
 		cn = "神秘运势之风",
+		tw = "神​秘命​運​之​風",
 	},
 	description = {
 		en = "Earn bonus experience and reputation on your adventures in Azeroth, and find mysterious fortunes in the Dragon Isles and Khaz Algar!",
 		cn = "在艾泽拉斯冒险可以获得额外的经验值和声望，还能在巨龙群岛和卡兹阿加找到神秘的鸿运之礼！",
+		tw = "在艾澤拉斯冒險可以獲得額外的經驗與聲望，還能在巨龍群島以及卡茲阿爾加找到神秘​的​背袋！",
 	},
 });
