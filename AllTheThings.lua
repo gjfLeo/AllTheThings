a5c793a6146378ccaae6f3944915bba85c946f70
ImUnicke <ImUnicke+github@gmail.com>
2025-04-30 00:44:41 -0600

    [Logic] Retail: Fix command name

--- AllTheThings.lua
+++ AllTheThings.lua
@@ -7220,7 +7220,7 @@ local function InitDataCoroutine()
 					count = count + 1
 				end
 				app.print("Removed & Backed up Duplicate Data of Current Character:",character.text,guid,"[You have",count,"total character backups]")
-				app.print("Use '/att removed-deleted-character-backups help' for more info")
+				app.print("Use '/att remove-deleted-character-backups help' for more info")
 			end
 			for _,guid in ipairs(toClean) do
 				app.FunctionRunner.Run(cleanCharacterFunc, guid);
@@ -7228,7 +7228,7 @@ local function InitDataCoroutine()
 		end
 
 		-- Allows removing the character backups that ATT automatically creates for duplicated characters which are replaced by new ones
-		app.ChatCommands.Add("removed-deleted-character-backups", function(args)
+		app.ChatCommands.Add("remove-deleted-character-backups", function(args)
 			local backups = 0
 			for guid,char in pairs(accountWideData._CharacterBackups) do
 				backups = backups + 1
@@ -7237,7 +7237,7 @@ local function InitDataCoroutine()
 			app.print("Cleaned up",backups,"character backups!")
 			return true
 		end, {
-			"Usage : /att removed-deleted-character-backups",
+			"Usage : /att remove-deleted-character-backups",
 			"Allows permanently removing all deleted character backup data",
 			"-- ATT removes and cleans out character-specific cached data which is stored by a character with the same Name-Realm as the logged-in character but a different character GUID. If you find yourself creating and deleting a lot of repeated characters, this will clean up those characters' data backups",
 		})
