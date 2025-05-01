1c407132194cbb998c2634758b530dcdce2e0a47
Myrhial <8618272+ninaarens@users.noreply.github.com>
2025-05-01 20:32:27 +0200

    Update PTR Wago files, fix batch updater script

--- .contrib/.wago/PTR/Update Wago PTR files.bat
+++ .contrib/.wago/PTR/Update Wago PTR files.bat
@@ -1,11 +1,13 @@
 @echo off
 @REM Download new file versions
 call :download Achievement
+call :download ArtifactAppearance
 call :download Criteria
 call :download CriteriaTree
 call :download GlyphProperties
 call :download Item
 call :download ItemEffect
+call :download ItemModifiedAppearance
 call :download ItemXItemEffect
 call :download ItemSearchName
 call :download ModifierTree
