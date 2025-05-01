e2429c80b7d0bd0c3093c72e195af39f58a4877c
ImUnicke <ImUnicke+github@gmail.com>
2025-05-01 01:09:56 -0600

    [Logic] Retail: Added '/att search|? [link]' which allows searching for a given data link and locating it within the respective ATT Window. If no link is provided, it will attempt to search for the given NPC target (Please report any bugs or issues experienced with this feature! Thanks)

--- src/Modules/Search.lua
+++ src/Modules/Search.lua
@@ -172,6 +172,7 @@ local function SearchByKindLink(link)
 end
 
 local function SearchForLink(link)
+	if not link then return end
 	local cleanlink = CleanLink(link)
 
 	-- real item links we should use the search by item link
@@ -439,3 +440,61 @@ function app:BuildTargettedSearchResponse(groups, field, value, drop, criteria)
 	end
 	return ClonedHierarchyGroups;
 end
+
+-- Allows a user to use /att search|? [link]
+-- to enable Debug Printing of Event messages
+app.ChatCommands.Add({"search","?"}, function(args)
+	local search = args[2]
+	if not search then
+		local guid = UnitGUID("target");
+		if guid then
+			search = "n:" .. select(6, ("-"):split(guid));
+		end
+	end
+
+	local results = SearchForLink(search)
+	if not results or #results == 0 then
+		app.print("No results found for",search)
+		return
+	end
+
+	-- expand the hierarchy to each search result
+	local DGR = app.DirectGroupRefresh
+	local GetRelative = app.GetRelativeRawWithField
+	local window
+	local o
+	for i = 1,#results do
+		o = results[i]
+		if not window then
+			-- find the containing window
+			window = GetRelative(o, "window")
+
+			-- open the containing Window
+			window:SetVisible(true)
+
+			-- collapse all the groups
+			app.ExpandGroupsRecursively(window.data, false, true)
+		end
+		-- force the search results to be visible
+		o.forceShow = true
+		-- DGU them to chain visibility
+		DGR(o)
+		o = o.parent
+		while o do
+			o.expanded = true
+			o = o.parent
+		end
+	end
+
+	-- report results
+	local firstResult = results[1]
+	app.print("Found",#results,"results for",app:SearchLink(firstResult),"within",window.Suffix)
+
+	-- mark the window to scroll to the first result
+	window:ScrollTo(firstResult.key, firstResult[firstResult.key])
+
+	return true
+end, {
+	"Usage : /att [search|?] [link]",
+	"Allows performing a search against ATT data and navigating to the found result(s)",
+})
