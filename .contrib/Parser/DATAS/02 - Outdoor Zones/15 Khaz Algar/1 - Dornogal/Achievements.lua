8a01e5cd95d166ebd4e537952a462d5f72394969
Myrhial <8618272+ninaarens@users.noreply.github.com>
2025-05-01 22:49:35 +0200

    Some more achievements so they are all sourced for the first 11.1.7 build

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Achievements.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Achievements.lua
@@ -8,6 +8,19 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, 
 				["description"] = "Stand at the coordinates and let the machine crush you.",
 				["coord"] = { 50.0, 62.0, DORNOGAL },	-- The Forgegrounds
 			}),
+			-- TODO: These might need to be moved, the quest starts in Dorn but that might not be the best home.
+			a(ach(41818, {	-- Rise of the Red Dawn (A)
+				["timeline"] = { ADDED_11_1_7 },
+				["groups"] = {
+					-- TODO: Title: of Stromgarde
+				},
+			})),
+			h(ach(41820, {	-- Rise of the Red Dawn (H)
+				["timeline"] = { ADDED_11_1_7 },
+				["groups"] = {
+					-- TODO: Title: of Hammerfall
+				},
+			})),
 		}),
 	}),
 })));
