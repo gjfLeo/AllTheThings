2343e10cdd0ee94574cc01a11e3c39ab3555a93d
ImUnicke <ImUnicke+github@gmail.com>
2025-05-12 12:21:30 -0600

    [DB] Removed NYI green item from PTR from AK treasure

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Azj-Kahet/Treasures.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Azj-Kahet/Treasures.lua
@@ -100,9 +100,6 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, 
 					{ 50.1, 50.7, AZJ_KAHET },
 					{ 51.8, 29.9, AZJ_KAHET },
 				},
-				["g"] = {
-					i(220504),	-- Deep-Dweller's Orb of the Harmonious
-				},
 			}),
 			o(444797, {	-- Threadspun Cache
 				["coords"] = {
