8fa598b4f15874cf9d161658c3ba94cb256c59c5
Myrhial <8618272+ninaarens@users.noreply.github.com>
2025-05-14 14:47:09 +0200

    Ashes to Ashes achievement removed, probably temporary as it was borked anyway?

--- .contrib/Parser/DATAS/06 - Expansion Features/07 - Legion/Achievements.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/07 - Legion/Achievements.lua
@@ -3,22 +3,23 @@
 -------------------------------------------------------------------
 root(ROOTS.ExpansionFeatures, expansion(EXPANSION.LEGION, {
 	n(ACHIEVEMENTS, {
-		ach(42028, {	-- Ashes to Ashes
-			["timeline"] = { ADDED_11_1_7 },
-			-- Meta Achievement
-			["sym"] = {{"meta_achievement",
-				42032,	-- Antorus, the Burning Throne
-				42114,	-- Broken Memories
-				11164,	-- Legion Dungeon Hero
-				42029,	-- The Emerald Nightmare
-				42030,	-- The Nighthold
-				42031,	-- Tomb of Sargeras
-				11394,	-- Trial of Valor
-				-- TODO: The rest are BFA achievements, this is probably unfinished work
-			}},
-			["groups"] = {
-				-- TODO: Mount Reward: Rites of the Felsoul
-			},
-		})
+		-- TODO: Removed in 11.1.7.60799 but I have a hunch it'll be back, they probably noticed it was broken, please don't yeet this file or data just yet please
+		-- ach(42028, {	-- Ashes to Ashes
+		-- 	["timeline"] = { ADDED_11_1_7 },
+		-- 	-- Meta Achievement
+		-- 	["sym"] = {{"meta_achievement",
+		-- 		42032,	-- Antorus, the Burning Throne
+		-- 		42114,	-- Broken Memories
+		-- 		11164,	-- Legion Dungeon Hero
+		-- 		42029,	-- The Emerald Nightmare
+		-- 		42030,	-- The Nighthold
+		-- 		42031,	-- Tomb of Sargeras
+		-- 		11394,	-- Trial of Valor
+		-- 		-- TODO: The rest are BFA achievements, this is probably unfinished work
+		-- 	}},
+		-- 	["groups"] = {
+		-- 		-- TODO: Mount Reward: Rites of the Felsoul
+		-- 	},
+		-- }),
 	}),
 }))
