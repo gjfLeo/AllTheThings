gjfLeo/AllTheThings

commit 1af5aab5b1ef5ac6c4360e7d074f271a0190cc1b

Argus: "Fix" a lot of contrib error reports by adding relevant maps to MapPrecisionOverrides
- also added a couple quest coordinates

.contrib/Parser/DATAS/02 - Outdoor Zones/09 Argus/1 - Vindicaar.lua
@@ -345,7 +345,10 @@ root(ROOTS.Zones, m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
 					["sourceQuests"] = { 48200 },	-- Securing a Foothold
 					["altQuests"] = { 48202 },	-- Reinforce the Veiled Den — you get one of two quests depending on which camp is up at the time
 					["provider"] = { "n", 126954 },	-- High Exarch Turalyon
-					["coord"] = { 34.4, 57.2, THE_VINDICAAR_KROKUUN_UPPER },
+					["coords"] = {
+						{ 33.6, 58.3, THE_VINDICAAR_ANTORAN_WASTES_UPPER },
+						{ 34.4, 57.2, THE_VINDICAAR_KROKUUN_UPPER },
+					},
 				}),
 				q(48202, {	-- Reinforce the Veiled Den
 					["sourceQuests"] = { 48200 },	-- Securing a Foothold
@@ -441,7 +444,11 @@ root(ROOTS.Zones, m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
 				q(48277, {	-- The Speaker Seeks
 					["sourceQuests"] = { 48272 },	-- Visions of Fear
 					["provider"] = { "n", 124312 },	-- High Exarch Turalyon
-					["coord"] = { 40.1, 25.6, THE_VINDICAAR_KROKUUN_UPPER },
+					["coord"] = {
+						{ 33.5, 58.3, THE_VINDICAAR_ANTORAN_WASTES_UPPER },
+						{ 45.9, 24.1, THE_VINDICAAR_EREDATH_UPPER },
+						{ 39.1, 24.9, THE_VINDICAAR_KROKUUN_UPPER },
+					},
 				}),
 				q(47287, {	-- The Vindicaar Matrix Core
 					["sourceQuests"] = { 47743 },	-- Child of Light and Shadow

.contrib/Parser/DATAS/02 - Outdoor Zones/09 Argus/Eredath/Quests.lua
@@ -235,6 +235,10 @@ root(ROOTS.Zones, m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
 			q(48445, {	-- The Ruins of Oronaar
 				["provider"] = { "n", 121230 },	-- Alleria Windrunner
 				["sourceQuests"] = { 48443 },	-- Supplying Krokuun
+				["coords"] = {
+					{ 38.8, 25.6, THE_VINDICAAR_KROKUUN_UPPER },
+					{ 45.5, 24.6, THE_VINDICAAR_EREDATH_UPPER },
+				},
 			}),
 			q(47218, {	-- The Shadowguard Incursion
 				["sourceQuests"] = { 47203 },	-- Throwing Shade

src/Modules/Contributor.lua
@@ -112,6 +112,13 @@ end
 -- 5) That number goes into this table for the mapID
 local MapPrecisionOverrides = {
 	 [629] = 3,	-- Aegwynn's Gallery
+	 [831] = 4,	-- The Vindicaar, Krokuun Upper
+	 [832] = 4,	-- The Vindicaar, Krokuun Lower
+	 [883] = 4,	-- The Vindicaar, Eredath Upper
+	 [884] = 4,	-- The Vindicaar, Eredath Lower
+	 [886] = 4,	-- The Vindicaar, Antoran Wastes Upper
+	 [887] = 4,	-- The Vindicaar, Antoran Wastes Lower
+	 [940] = 4,	-- The Vindicaar
 	[1164] = 3,	-- Dazar'alor
 	[1176] = 3,	-- Breath Of Pa'ku
 	[1177] = 3,	-- Breath Of Pa'ku

commit ed89e3a7b98dbd67a80b961687d82b921642a139

Argus: Fix a typo introduced in 1af5aab5b1ef5ac6c4360e7d074f271a0190cc1b

.contrib/Parser/DATAS/02 - Outdoor Zones/09 Argus/1 - Vindicaar.lua
@@ -444,7 +444,7 @@ root(ROOTS.Zones, m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
 				q(48277, {	-- The Speaker Seeks
 					["sourceQuests"] = { 48272 },	-- Visions of Fear
 					["provider"] = { "n", 124312 },	-- High Exarch Turalyon
-					["coord"] = {
+					["coords"] = {
 						{ 33.5, 58.3, THE_VINDICAAR_ANTORAN_WASTES_UPPER },
 						{ 45.9, 24.1, THE_VINDICAAR_EREDATH_UPPER },
 						{ 39.1, 24.9, THE_VINDICAAR_KROKUUN_UPPER },

.contrib/Parser/DATAS/02 - Outdoor Zones/09 Argus/Eredath/Quests.lua
@@ -236,8 +236,8 @@ root(ROOTS.Zones, m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
 				["provider"] = { "n", 121230 },	-- Alleria Windrunner
 				["sourceQuests"] = { 48443 },	-- Supplying Krokuun
 				["coords"] = {
-					{ 38.8, 25.6, THE_VINDICAAR_KROKUUN_UPPER },
 					{ 45.5, 24.6, THE_VINDICAAR_EREDATH_UPPER },
+					{ 38.8, 25.6, THE_VINDICAAR_KROKUUN_UPPER },
 				},
 			}),
 			q(47218, {	-- The Shadowguard Incursion