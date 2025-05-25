61168f2c6afe52835b8a02e2449ba9d17e2124a2
Crieve <dylanfortune@live.com>
2025-05-25 09:59:11 -0700

    The Master Planner's Blueprints are also included as PQI for the follow up quest.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/Nagrand.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/03 Outland/Nagrand.lua
@@ -1325,9 +1325,13 @@ root(ROOTS.Zones, {
 						},
 					}),
 					q(10004, {	-- Patience and Understanding
-						["qg"] = 18417,	-- Altruis the Sufferer
+						["providers"] = {
+							{ "n", 18417 },	-- Altruis the Sufferer
+							{ "i", 25751 },	-- The Master Planner's Blueprints (PQI!)
+						},
 						["sourceQuest"] = 10001,	-- The Master Planner
 						["coord"] = { 27.4, 42.0, NAGRAND },
+						["maps"] = { SHATTRATH_CITY },
 					}),
 					q(9797, {	-- Reinforcements for Garadar
 						["qg"] = 18091,	-- Messenger Gazgrigg
