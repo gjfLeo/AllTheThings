1efd177877862f18ea55b5152dbf4434e6bfb3ee
Goldenshacal <goldenshacal@gmail.com>
2025-06-13 18:31:31 +0200

    added SE tier set items. no cost attached but better than unsorted

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/3.5 Scarlet Enclave.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/3.5 Scarlet Enclave.lua
@@ -12,9 +12,238 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
 		-- ["sourceQuest"] = 86970,
 		["groups"] = {
 			n(QUESTS),
-			d(DIFFICULTY.RAID.NORMAL, {
-				["sharedDescription"] = "[TBC]\nIf the boss loot that is not in the boss's loot table, @NORPG on Discord.",
+			d(DIFFICULTY.SOD.PLAYER20, {
 				["groups"] = {
+					n(11036, {	-- Leonid Barthalomew the Revered <The Argent Dawn>
+						["coords"] = { 
+							{ 81.6, 57.8, EASTERN_PLAGUELANDS },	-- Leonid Barthalomew the Revered <The Argent Dawn>
+							{ 98.6, 84.0, EASTERN_PLAGUELANDS },	-- Bryon Steelblade, but he sells the exact same items
+						},
+						["groups"] = {
+							cl(DRUID, {
+								-- DPS (AGILITY)
+								i(240060),	-- Waywatcher Wraps
+								i(240061),	-- Waywatcher Sash
+								i(240062),	-- Waywatcher Shoulderpads
+								i(240063),	-- Waywatcher Trousers
+								i(240064),	-- Waywatcher Cowl
+								i(240065),	-- Waywatcher Grips
+								i(240066),	-- Waywatcher Stompers
+								i(240067),	-- Waywatcher Vest
+								-- DPS (Intellect)
+								i(240068),	-- Waywatcher Bindings
+								i(240069),	-- Waywatcher Cord
+								i(240070),	-- Waywatcher Mantle
+								i(240071),	-- Waywatcher Kilt
+								i(240072),	-- Waywatcher Hood
+								i(240073),	-- Waywatcher Mitts
+								i(240074),	-- Waywatcher Sandals
+								i(240075),	-- Waywatcher Leathers
+								-- Healer
+								i(240084),	-- Waywatcher Wristguards
+								i(240085),	-- Waywatcher Girdle
+								i(240086),	-- Waywatcher Spaulders
+								i(240087),	-- Waywatcher Legguards
+								i(240088),	-- Waywatcher Headpiece
+								i(240089),	-- Waywatcher Handguards
+								i(240090),	-- Waywatcher Boots
+								i(240091),	-- Waywatcher Tunic
+								-- Tank
+								i(240076),	-- Waywatcher Bracers
+								i(240077),	-- Waywatcher Waistguard
+								i(240078),	-- Waywatcher Pauldrons
+								i(240079),	-- Waywatcher Leggings
+								i(240080),	-- Waywatcher Headdress
+								i(240081),	-- Waywatcher Gauntlets
+								i(240082),	-- Waywatcher Sabatons
+								i(240083),	-- Waywatcher Armor
+							}),
+							cl(HUNTER, {
+								-- Melee
+								i(239533),	-- Dawnstalker Leggings
+								i(239534),	-- Dawnstalker Pauldrons
+								i(239535),	-- Dawnstalker Belt
+								i(239536),	-- Dawnstalker Vambraces
+								i(239529),	-- Dawnstalker Breastplate
+								i(239530),	-- Dawnstalker Greaves
+								i(239531),	-- Dawnstalker Gauntlets
+								i(239532),	-- Dawnstalker Viso
+								-- Ranged
+								i(239537),	-- Dawnstalker Boots
+								i(239538),	-- Dawnstalker Girdle
+								i(239539),	-- Dawnstalker Handguards
+								i(239541),	-- Dawnstalker Legguards
+								i(239542),	-- Dawnstalker Spaulders
+								i(239543),	-- Dawnstalker Tunic
+								i(239544),	-- Dawnstalker Wristguards
+							}),
+							cl(MAGE, {	
+								-- DPS
+								i(240052),	-- Fireleaf Bindings
+								i(240053),	-- Fireleaf Belt
+								i(240054),	-- Fireleaf Shoulderpads
+								i(240055),	-- Fireleaf Leggings
+								i(240056),	-- Fireleaf Circlet
+								i(240057),	-- Fireleaf Gloves
+								i(240058),	-- Fireleaf Sandals
+								i(240059),	-- Fireleaf Robe
+								-- Healer
+								i(240044),	-- Fireleaf Wristwraps
+								i(240045),	-- Fireleaf Waistguard
+								i(240046),	-- Fireleaf Mantle
+								i(240047),	-- Fireleaf Pants
+								i(240048),	-- Fireleaf Hood
+								i(240049),	-- Fireleaf Mitts
+								i(240050),	-- Fireleaf Boots
+								i(240051),	-- Fireleaf Garb
+							}),
+							cl(PALADIN, {
+								-- DPS
+								i(240023),	-- Inquisition Vambraces
+								i(240024),	-- Inquisition Belt
+								i(240025),	-- Inquisition Shoulderplates
+								i(240026),	-- Inquisition Leggings
+								i(240027),	-- Inquisition Helmet
+								i(240028),	-- Inquisition Gloves
+								i(240029),	-- Inquisition Greaves
+								i(240030),	-- Inquisition Breastplate
+								-- Healer
+								i(240020),	-- Inquisition Legguards
+								i(240021),	-- Inquisition Spaulders
+								i(240022),	-- Inquisition Boots
+								i(240039),	-- Inquisition Tunic
+								i(240040),	-- Inquisition Headpiece
+								i(240041),	-- Inquisition Handguards
+								i(240042),	-- Inquisition Wristguards
+								i(240043),	-- Inquisition Girdle
+								-- Tank
+								i(240031),	-- Inquisition Bracers
+								i(240032),	-- Inquisition Waistguard
+								i(240033),	-- Inquisition Pauldrons
+								i(240034),	-- Inquisition Legplates
+								i(240035),	-- Inquisition Crown
+								i(240036),	-- Inquisition Gauntlets
+								i(240037),	-- Inquisition Sabatons
+								i(240038),	-- Inquisition Chestguard
+							}),
+							cl(PRIEST, {
+								i(239565),	-- Garb of Revelation
+								i(239572),	-- Boots of Revelation
+								i(239574),	-- Hands of Revelation
+								i(239575),	-- Crown of Revelation
+								i(239577),	-- Pants of Revelation
+								i(239581),	-- Mantle of Revelation
+								i(239582),	-- Girdle of Revelation
+								i(239583),	-- Wrists of Revelation
+								-- Healer
+								i(239584),	-- Gloves of Revelation
+								i(239585),	-- Circlet of Revelation
+								i(239586),	-- Shoulderpads of Revelation
+								i(239587),	-- Leggings of Revelation
+								i(239588),	-- Bindings of Revelation
+								i(239589),	-- Sandals of Revelation
+								i(239590),	-- Belt of Revelation
+								i(239591),	-- Robe of Revelation
+							}),
+							cl(ROGUE, {
+								i(239555),	-- Duskwraith Bracers
+								i(239556),	-- Duskwraith Waistguard
+								i(239557),	-- Duskwraith Gauntlets
+								i(239558),	-- Duskwraith Sabatons
+								i(239559),	-- Duskwraith Pauldrons
+								i(239560),	-- Duskwraith Helmet
+								i(239561),	-- Duskwraith Legplates
+								i(239562),	-- Duskwraith Breastplate
+								-- Tank
+								i(239547),	-- Duskwraith Wristguards
+								i(239548),	-- Duskwraith Chestguard
+								i(239549),	-- Duskwraith Grips
+								i(239550),	-- Duskwraith Mask
+								i(239551),	-- Duskwraith Leggings
+								i(239552),	-- Duskwraith Mantle
+								i(239553),	-- Duskwraith Treads
+								i(239554),	-- Duskwraith Belt
+							}),
+							cl(SHAMAN, {
+								i(240128),	-- Soulcrusher Armor
+								i(240129),	-- Soulcrusher Treads
+								i(240130),	-- Soulcrusher Grips
+								i(240131),	-- Soulcrusher Helmet
+								i(240132),	-- Test Staff
+								i(240134),	-- Soulcrusher Leggings
+								i(240135),	-- Soulcrusher Epaulets
+								i(240136),	-- Soulcrusher Belt
+								i(240137),	-- Soulcrusher Vambraces
+								-- DPS (Intellect)
+								i(240109),	-- Soulcrusher Embrace
+								i(240110),	-- Soulcrusher Greaves
+								i(240122),	-- Soulcrusher Mitts
+								i(240123),	-- Soulcrusher Crown
+								i(240124),	-- Soulcrusher Kilt
+								i(240125),	-- Soulcrusher Mantle
+								i(240126),	-- Soulcrusher Cord
+								i(240127),	-- Soulcrusher Bindings
+								-- Healer
+								i(240092),	-- Soulcrusher Tunic
+								i(240093),	-- Soulcrusher Boots
+								i(240095),	-- Soulcrusher Handguards
+								i(240096),	-- Soulcrusher Headpiece
+								i(240097),	-- Soulcrusher Legguards
+								i(240098),	-- Soulcrusher Spaulders
+								i(240099),	-- Soulcrusher Girdle
+								i(240100),	-- Soulcrusher Wristguards
+								-- Tank
+								i(240101),	-- Soulcrusher Chestguard
+								i(240102),	-- Soulcrusher Sabatons
+								i(240103),	-- Soulcrusher Gloves
+								i(240104),	-- Soulcrusher Faceguard
+								i(240105),	-- Soulcrusher Legplates
+								i(240106),	-- Soulcrusher Pauldrons
+								i(240107),	-- Soulcrusher Waistguard
+								i(240108),	-- Soulcrusher Bracers
+							}),
+							cl(WARLOCK, {
+								-- DPS
+								i(240138),	-- Heretic Robe
+								i(240139),	-- Heretic Sandals
+								i(240140),	-- Heretic Gloves
+								i(240141),	-- Heretic Cowl
+								i(240142),	-- Heretic Leggings
+								i(240143),	-- Heretic Shoulderpads
+								i(240144),	-- Heretic Belt
+								i(240145),	-- Heretic Bindings
+								-- Tank
+								i(240146),	-- Heretic Garb
+								i(240147),	-- Heretic Handguards
+								i(240148),	-- Heretic Hood
+								i(240149),	-- Heretic Pants
+								i(240150),	-- Heretic Mantle
+								i(240151),	-- Heretic Waistguard
+								i(240152),	-- Heretic Wristguards
+								i(240153),	-- Heretic Boots
+							}),
+							cl(WARRIOR, {
+								-- DPS
+								i(239512),	-- Lightbreaker Wrists
+								i(239513),	-- Lightbreaker Belt
+								i(239514),	-- Lightbreaker Grips
+								i(239515),	-- Lightbreaker Greaves
+								i(239516),	-- Lightbreaker Shoulders
+								i(239517),	-- Lightbreaker Greathelm
+								i(239518),	-- Lightbreaker Tassets
+								i(239519),	-- Lightbreaker Cuirass
+								-- Tank
+								i(239520),	-- Lightbreaker Bracers
+								i(239521),	-- Lightbreaker Waistguard
+								i(239522),	-- Lightbreaker Gauntlets
+								i(239523),	-- Lightbreaker Sabatons
+								i(239524),	-- Lightbreaker Pauldrons
+								i(239525),	-- Lightbreaker Helmet
+								i(239526),	-- Lightbreaker Legplates
+								i(239527),	-- Lightbreaker Breastplate
+							}),
+						},
+					}),
 					n(COMMON_BOSS_DROPS),
 					e(3185, {	-- Balnazzar 
 						["creatureID"] = 240811,
