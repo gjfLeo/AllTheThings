15869563df6cb26caf4b9213fe732ae3d2190ef0
milotic <al0305@hotmail.com>
2025-06-17 22:44:17 -0600

    Fixed translations for Vizier's Appreciation, Wow Anniversary, and Incognitro Motorcycle objects in several languages. (#2039)
    * Darkmoon Fortune Card translations was already in wowhead
    
    * Vizier's Appreciation fixed the traslation in korean y portuguese and added in mx and tw
    
    * Abandoned Beach Chair added traslation of mx y tw
    
    * Inert Peculiar Key translation to every language
    Hear Historic Tales at the Storyteller Stage translations to mx and tw
    Gerald to mx and tw
    Soggy Celebration Crate to mx and tw
    red hair to mx and tw
    Strange Torch to mx and tw
    Torn Note to it, mx, pt and tw
    Mounted Spyglass to it, mx, pt and tw
    Jeweled Eye to mx and tw
    Half-Buried Chest to mx and tw
    Dirt-Caked Celebration Crate to mx and tw
    Water-Resistant Receipt of Sale to mx and tw

--- .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638661865595182851.lua
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638661865595182851.lua
@@ -4,14 +4,16 @@
 		model = 197230,
 		text = {
 			en = "Abandoned Beach Chair",
-			es = "Tumbona de playa abandonada",
 			de = "Verlassener Strandstuhl",
+			es = "Tumbona de playa abandonada",
 			fr = "Chaise de plage abandonnée",
 			it = "Sdraio Abbandonata",
+			mx = "Silla de playa abandonada",
 			pt = "Cadeira de Praia Abandonada",
 			ru = "Брошенный пляжный шезлонг",
 			ko = "버려진 해변 의자",
 			cn = "被遗弃的沙滩椅",
+			tw = "被遺棄的海灘椅",
 		},
 	},
 })
