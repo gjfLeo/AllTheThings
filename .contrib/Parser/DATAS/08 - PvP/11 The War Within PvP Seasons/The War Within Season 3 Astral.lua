db4f4df1db9b630daca8f6f7d797d63e7f1c3704
Myrhial <8618272+ninaarens@users.noreply.github.com>
2025-06-20 19:57:15 +0200

    11.2 PVP achievements sorted

--- .contrib/Parser/DATAS/08 - PvP/11 The War Within PvP Seasons/The War Within Season 3 Astral.lua
+++ .contrib/Parser/DATAS/08 - PvP/11 The War Within PvP Seasons/The War Within Season 3 Astral.lua
@@ -1,78 +1,47 @@
 -----------------------------------------------
 --      P L A Y E R   V S   P L A Y E R      --
 -----------------------------------------------
---[[
-root(ROOTS.PVP, pvp(expansion(EXPANSION.TWW, bubbleDownSelf({ ["timeline"] = { ADDED_11_1_0 } }, {
-	n(SEASON_PRIZED, {
-		n(ACHIEVEMENTS, bubbleDownSelf({ ["timeline"] = { ADDED_11_1_0_SEASONSTART, REMOVED_11_2_0 } }, {
-			ach(41128, {	-- Prized Combatant [A]
+root(ROOTS.PVP, pvp(expansion(EXPANSION.TWW, bubbleDownSelf({ ["timeline"] = { ADDED_11_2_0 } }, {
+	n(SEASON_ASTRAL, {
+		n(ACHIEVEMENTS, bubbleDownSelf({ ["timeline"] = { ADDED_11_2_0_SEASONSTART, REMOVED_12_0_0 } }, {
+			ach(41048),	-- [11.2 PVP] Weapons of Conquest
+			ach(42042, {
 				["races"] = ALLIANCE_ONLY,
 				["collectible"] = false,
-			}),
-			ach(41129, {	-- Prized Combatant [H]
+			}),	-- Astral Combatant
+			ach(42043, {
 				["races"] = HORDE_ONLY,
 				["collectible"] = false,
-			}),
-			ach(41020),		-- Combatant I: The War Within Season 2
-			ach(41021),		-- Combatant II: The War Within Season 2
-			ach(41022),		-- Challenger I: The War Within Season 2
-			ach(41023),		-- Challenger II: The War Within Season 2
-			ach(41047, {	-- Prized Weapons of Conquest
-				i(232615, {	-- Prized Gladiator's Coin Pouch
-					i(230852, {	-- Prized Gladiator's Weapon Token
-					}),
-				}),
-			}),
-			ach(41016),		-- Rival I: The War Within Season 2
-			ach(41017, {	-- Rival II: The War Within Season 2
-				ill(7640),	-- Jackpot (ILLUSION!)
-			}),
-			ach(41018, {	-- Duelist: The War Within Season 2
-				i(231638),	-- Prized Gladiator's Prestigious Cloak
-			}),
-			ach(41019),		-- Elite: The War Within Season 2
-			ach(41032, {	-- Gladiator: The War Within Season 2
-				i(229987),	-- Prized Gladiator's Fel Bat (MOUNT!)
-			}),
-			ach(41362),		-- Prized Gladiator's Fel Bat
-			ach(41354, {	-- Prized Gladiator: The War Within Season 2
-				title(608),	-- Prized Gladiator <Name>
-			}),
+			}),	-- Astral Combatant
+			ach(41028),	-- Combatant I: The War Within Season 3
+			ach(41029),	-- Combatant II: The War Within Season 3
+			ach(41030),	-- Challenger I: The War Within Season 3
+			ach(41031),	-- Challenger II: The War Within Season 3
+			ach(41026),	-- Duelist: The War Within Season 3
+			ach(41027),	-- Elite: The War Within Season 3
+			ach(41049),	-- Gladiator: The War Within Season 3
+			ach(41024),	-- Rival I: The War Within Season 3
+			ach(41025),	-- Rival II: The War Within Season 3
 			-- RBG
-			ach(41361, {	-- Hero of the Alliance: Prized
-				["races"] = ALLIANCE_ONLY,
+			ach(42036),	-- Astral Gladiator: The War Within Season 3
+			ach(42039),	-- Astral Gladiator's Fel Bat
+			ach(42035),	-- Astral Marshal: The War Within Season 3
+			ach(42034),	-- Astral Warlord: The War Within Season 3
+			ach(42038, {	-- Hero of the Alliance: Astral
+				["races"]= ALLIANCE_ONLY,
 			}),
-			ach(41360, {	-- Hero of the Horde: Prized
-				["races"] = HORDE_ONLY,
+			ach(42037, {	-- Hero of the Horde: Astral
+				["races"]= HORDE_ONLY,
 			}),
-			ach(41357, {	-- Prized Marshal: The War Within Season 2
-				["races"] = ALLIANCE_ONLY,
-				["g"] = {
-					title(610),	-- Prized Marshal <Name>
-				},
-			}),
-			ach(41356, {	-- Prized Warlord: The War Within Season 2
-				["races"] = HORDE_ONLY,
-				["g"] = {
-					title(609),	-- Prized Warlord <Name>
-				},
-			}),
-			ach(41363, {	-- The Strategist: The War Within Season 2
-				i(235519),	-- Prized Legend's Pennant (TOY!)
-			}),
+			ach(42024),	-- Strategist: The War Within Season 3
 			-- Solo
-			ach(41358, {	-- Legend: The War Within Season 2
-				i(235519),	-- Prized Legend's Pennant (TOY!)
-			}),
-			ach(41355, {	-- Prized Legend: The War Within Season 2
-				title(611),	-- Prized Legend <Name>
-			}),
-			ach(41359, {	-- Battle Mender: The War Within Season 2
-				["classes"] = HEALERS,
-			}),
+			ach(42023),	-- Legend: The War Within Season 3
+			ach(42044),	-- Battle Mender: The War Within Season 3
+			ach(42033),	-- Astral Legend: The War Within Season 3
 			-- Fashion
-			ach(41595),	-- Prized Guise
+			ach(42800),	-- Astral Attire
 		})),
+		--[[
 		filter(MOUNTS, bubbleDownSelf({ ["timeline"] = { ADDED_11_1_0_SEASONSTART, REMOVED_11_2_0 } }, {
 			i(229989, {	-- Vicious Electro Eel [A] (MOUNT!)
 				["races"] = ALLIANCE_ONLY,
@@ -780,6 +749,6 @@ root(ROOTS.PVP, pvp(expansion(EXPANSION.TWW, bubbleDownSelf({ ["timeline"] = { A
 				},
 			}),
 		})),
+		]]--
 	}),
 }))));
---]]
---
+++ .contrib/Parser/lib/Headers/PvP/Arena Seasons/Season 40 - Astral Gladiator (TWW).lua
@@ -0,0 +1,7 @@
+SEASON_ASTRAL = createHeader({
+	readable = "Astral Gladiator: The War Within Season 3",
+	icon = 6124221,
+	text = {
+		en = WOWAPI_GetAchievementName(42036),
+	},
+});
