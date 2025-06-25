5c35e650ed53a0021c12e3b6e2435c38ff4fcc46
Exodius <nikolakrstic987@gmail.com>
2025-06-25 02:11:35 +0200

    PTR Parse

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638864068643527337.lua
@@ -0,0 +1,20 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[517000] = {
+		readable = "Tazavesh Trash",
+		model = 3930675,
+		text = {
+			en = "Tazavesh Trash",
+			es = "Tazavesh Trash",
+			mx = "Tazavesh Trash",
+			de = "Tazavesh Trash",
+			fr = "Tazavesh Trash",
+			it = "Tazavesh Trash",
+			pt = "Tazavesh Trash",
+			ru = "Tazavesh Trash",
+			ko = "Tazavesh Trash",
+			cn = "Tazavesh Trash",
+			tw = "Tazavesh Trash",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
