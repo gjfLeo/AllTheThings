63316848d7847ff74a6c6ec201e1474bfd22e7d0
milotic <al0305@hotmail.com>
2025-07-01 01:30:57 -0600

    More translations of objects in all languages (#2066)
    * More Undermine object translations
    
    * add mx and tw to the commit 236f053

--- .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638720791280866188.lua
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/DynamicObjectDB_638720791280866188.lua
@@ -4,14 +4,16 @@
 		model = 304794,
 		text = {
 			en = "Dauntless Draught",
-			es = "Jarabe intrépido",
 			de = "Trunk der Furchtloser Funke",
+			es = "Jarabe intrépido",
+			mx = "Jarabe intrépido",
 			fr = "Breuvage de l’Audacieux",
 			it = "Spina dell'Impavida",
 			pt = "Grogue Inabalável",
 			ru = "Храбрый хмель",
 			ko = "용맹주",
 			cn = "果敢饮剂",
+			tw = "無懼藥水",
 		},
 	},
 })
