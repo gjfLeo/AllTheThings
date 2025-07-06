96e9e4b356e4c027041337589eceb626b1b4f00c
Crieve <dylanfortune@live.com>
2025-07-05 17:20:58 -0700

    [Parser] Now cleaning up empty non-standalone headers in map data.

--- .contrib/Source Code/Parser/Export/Object Data/MapData.cs
+++ .contrib/Source Code/Parser/Export/Object Data/MapData.cs
@@ -16,6 +16,24 @@ namespace ATT
         public class MapData : ObjectData
         {
             /// <summary>
+            /// These are empty headers we know about.
+            /// </summary>
+            static Dictionary<string, bool> EMPTY_HEADER_WHITELIST = new Dictionary<string, bool>
+            {
+                { "Achievements", true },
+                { "Pet Battles", true },
+                { "Exploration", true },
+                { "Quests", true },
+                { "Rares", true },
+                { "Special", true },
+                { "Treasures", true },
+                { "Vendors", true },
+                { "World Quests", true },
+                { "Zone Drops", true },
+                { "Zone Rewards", true },
+            };
+
+            /// <summary>
             /// Build the object.
             /// This writes the primary field and removes it from the list of available fields.
             /// </summary>
@@ -26,6 +44,38 @@ namespace ATT
             {
                 if (data.TryGetValue("mapID", out long mapID))
                 {
+                    if (data.TryGetValue("g", out List<object> g))
+                    {
+                        for (int i = g.Count - 1; i >= 0; --i)
+                        {
+                            if (g[i] is Dictionary<string, object> element)
+                            {
+                                // Determine if this element is a custom header
+                                if (element.TryGetValue("npcID", out long npcID) && npcID < 0
+                                    && Framework.CustomHeaders.TryGetValue(npcID, out object o) && o is IDictionary<string, object> header)
+                                {
+                                    // If there's no relative groups or symlink on the object
+                                    if (!element.TryGetValue("g", out List<object> g2) || g2.Count < 1 && !element.ContainsKey("sym"))
+                                    {
+                                        // And the header has not been declared as standalone to not allow empty headers, then delete the object.
+                                        if (header.TryGetValue("standalone", out bool standalone) && !standalone)
+                                        {
+                                            if (header.TryGetValue("readable", out string readable) && !EMPTY_HEADER_WHITELIST.ContainsKey(readable))
+                                            {
+                                                EMPTY_HEADER_WHITELIST[readable] = true;
+                                                Console.WriteLine($"EMPTY HEADER {readable ?? "UNKNOWN"}");
+                                                Console.WriteLine($"  {MiniJSON.Json.Serialize(element)}");
+                                                Console.ReadLine();
+                                            }
+                                            g.RemoveAt(i);
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
                     // If we have a pet battle level available in Wago, let's apply it!
                     if (!data.ContainsKey("petBattleLvl") && WagoData.TryGetUiMapIDAssociations<UiMapAssignment>(mapID, out var associations))
                     {
