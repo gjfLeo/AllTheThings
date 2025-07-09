2ac26002b54b36ef7213da6e0237e0aab3108dca
ImUnicke <ImUnicke+github@gmail.com>
2025-07-09 14:52:14 -0600

    [Misc] Added more notes about using MAPID_MERGE_REPLACEMENTS

--- .contrib/Parser/DATAS/00 - DB/Parser Setup Data/MAPID_MERGE_REPLACEMENTS.lua
+++ .contrib/Parser/DATAS/00 - DB/Parser Setup Data/MAPID_MERGE_REPLACEMENTS.lua
@@ -5,6 +5,10 @@
 -- NYI maps are blanket ignored, but other maps are simply linked via 'maps' and are not directly sourced. This
 -- small set of mapID replacements tells parser that instead of merging into one of these maps
 
+-- Typically you'd add to this file when parser complains about a missing mapID:
+-- "WARN: Failed to merge data which requires a Source: [mapID]:[###]"
+-- but the missing map can't be sourced under NYI or Unsorted because it's used in 'maps' in available maps
+
 MAPID_MERGE_REPLACEMENTS = {
 	-- #if AFTER CATA
 	[224] = THE_CAPE_OF_STRANGLETHORN,
