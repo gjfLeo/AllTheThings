fed48c3e0d62442c17ca091d109fd4e9f4a95e4c
ImUnicke <ImUnicke+github@gmail.com>
2025-07-16 08:18:43 -0600

    [DB] Some prep data to allow the 'Primalist tokens to properly show their contained Item's catalyst results in tooltips

--- .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Forbidden Reach/Zone Rewards.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/14 Dragon Isles/The Forbidden Reach/Zone Rewards.lua
@@ -1,6 +1,9 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
+local CatalystableItem = setmetatable({}, { __index = function(t,key)
+	return { bonusID = 8822 }	-- bonusID signifies catalystID 3 when checked via ItemConversionDB
+end})
 root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_0_7 } }, {
 	m(THE_FORBIDDEN_REACH, {
 		n(ZONE_REWARDS, {
@@ -18,114 +21,114 @@ root(ROOTS.Zones, m(DRAGON_ISLES, bubbleDown({ ["timeline"] = { ADDED_10_0_7 } }
 			}),
 			n(BACK, {
 				i(203646, {	-- Primalist Cloak
-					iupgrade(199450, 14, 9344),	-- Cape of Raging Tempests
-					iupgrade(199451, 14, 9344),	-- Cloak of Raging Tempests
-					iupgrade(199452, 14, 9344),	-- Shroud of Raging Tempests
-					iupgrade(199449, 14, 9344),	-- Wrap of Raging Tempests
+					iupgrade(199450, 14, 9344, CatalystableItem.Item),	-- Cape of Raging Tempests
+					iupgrade(199451, 14, 9344, CatalystableItem.Item),	-- Cloak of Raging Tempests
+					iupgrade(199452, 14, 9344, CatalystableItem.Item),	-- Shroud of Raging Tempests
+					iupgrade(199449, 14, 9344, CatalystableItem.Item),	-- Wrap of Raging Tempests
 				}),
 			}),
 			filter(CLOTH, {
 				i(203635, {	-- Primalist Cloth Belt
-					iupgrade(199422, 14, 9344),	-- Cord of Raging Tempests
+					iupgrade(199422, 14, 9344, CatalystableItem.Item),	-- Cord of Raging Tempests
 				}),
 				i(203641, {	-- Primalist Cloth Boots
-					iupgrade(199418, 14, 9344),	-- Boots of Raging Tempests
+					iupgrade(199418, 14, 9344, CatalystableItem.Item),	-- Boots of Raging Tempests
 				}),
 				i(203632, {	-- Primalist Cloth Bracers
-					iupgrade(199423, 14, 9344),	-- Cuffs of Raging Tempests
+					iupgrade(199423, 14, 9344, CatalystableItem.Item),	-- Cuffs of Raging Tempests
 				}),
 				i(203616, {	-- Primalist Cloth Chestpiece
-					iupgrade(199417, 14, 9344),	-- Vestment of Raging Tempests
+					iupgrade(199417, 14, 9344, CatalystableItem.Item),	-- Vestment of Raging Tempests
 				}),
 				i(203642, {	-- Primalist Cloth Gloves
-					iupgrade(199419, 14, 9344),	-- Gloves of Raging Tempests
+					iupgrade(199419, 14, 9344, CatalystableItem.Item),	-- Gloves of Raging Tempests
 				}),
 				i(203612, {	-- Primalist Cloth Helm
-					iupgrade(199420, 14, 9344),	-- Hood of Raging Tempests
+					iupgrade(199420, 14, 9344, CatalystableItem.Item),	-- Hood of Raging Tempests
 				}),
 				i(203622, {	-- Primalist Cloth Leggings
-					iupgrade(199421, 14, 9344),	-- Leggings of Raging Tempests
+					iupgrade(199421, 14, 9344, CatalystableItem.Item),	-- Leggings of Raging Tempests
 				}),
 				i(203627, {	-- Primalist Cloth Spaulders
-					iupgrade(199424, 14, 9344),	-- Shoulderpads of Raging Tempests
+					iupgrade(199424, 14, 9344, CatalystableItem.Item),	-- Shoulderpads of Raging Tempests
 				}),
 			}),
 			filter(LEATHER, {
 				i(203637, {	-- Primalist Leather Belt
-					iupgrade(199429, 14, 9344),	-- Sash of Raging Tempests
+					iupgrade(199429, 14, 9344, CatalystableItem.Item),	-- Sash of Raging Tempests
 				}),
 				i(203638, {	-- Primalist Leather Boots
-					iupgrade(199425, 14, 9344),	-- Waders of Raging Tempests
+					iupgrade(199425, 14, 9344, CatalystableItem.Item),	-- Waders of Raging Tempests
 				}),
 				i(203630, {	-- Primalist Leather Bracers
-					iupgrade(199430, 14, 9344),	-- Bindings of Raging Tempests
+					iupgrade(199430, 14, 9344, CatalystableItem.Item),	-- Bindings of Raging Tempests
 				}),
 				i(203618, {	-- Primalist Leather Chestpiece
-					iupgrade(199431, 14, 9344),	-- Vest of Raging Tempests
+					iupgrade(199431, 14, 9344, CatalystableItem.Item),	-- Vest of Raging Tempests
 				}),
 				i(203645, {	-- Primalist Leather Gloves
-					iupgrade(199426, 14, 9344),	-- Handguards of Raging Tempests
+					iupgrade(199426, 14, 9344, CatalystableItem.Item),	-- Handguards of Raging Tempests
 				}),
 				i(203614, {	-- Primalist Leather Helm
-					iupgrade(199427, 14, 9344),	-- Cowl of Raging Tempests
+					iupgrade(199427, 14, 9344, CatalystableItem.Item),	-- Cowl of Raging Tempests
 				}),
 				i(203619, {	-- Primalist Leather Leggings
-					iupgrade(199428, 14, 9344),	-- Breeches of Raging Tempests
+					iupgrade(199428, 14, 9344, CatalystableItem.Item),	-- Breeches of Raging Tempests
 				}),
 				i(203629, {	-- Primalist Leather Spaulders
-					iupgrade(199432, 14, 9344),	-- Epaulets of Raging Tempests
+					iupgrade(199432, 14, 9344, CatalystableItem.Item),	-- Epaulets of Raging Tempests
 				}),
 			}),
 			filter(MAIL, {
 				i(203636, {	-- Primalist Mail Belt
-					iupgrade(199447, 14, 9344),	-- Cinch of Raging Tempests
+					iupgrade(199447, 14, 9344, CatalystableItem.Item),	-- Cinch of Raging Tempests
 				}),
 				i(203639, {	-- Primalist Mail Boots
-					iupgrade(199442, 14, 9344),	-- Striders of Raging Tempests
+					iupgrade(199442, 14, 9344, CatalystableItem.Item),	-- Striders of Raging Tempests
 				}),
 				i(203631, {	-- Primalist Mail Bracers
-					iupgrade(199448, 14, 9344),	-- Bracers of Raging Tempests
+					iupgrade(199448, 14, 9344, CatalystableItem.Item),	-- Bracers of Raging Tempests
 				}),
 				i(203617, {	-- Primalist Mail Chestpiece
-					iupgrade(199441, 14, 9344),	-- Chestguard of Raging Tempests
+					iupgrade(199441, 14, 9344, CatalystableItem.Item),	-- Chestguard of Raging Tempests
 				}),
 				i(203644, {	-- Primalist Mail Gloves
-					iupgrade(199443, 14, 9344),	-- Grips of Raging Tempests
+					iupgrade(199443, 14, 9344, CatalystableItem.Item),	-- Grips of Raging Tempests
 				}),
 				i(203613, {	-- Primalist Mail Helm
-					iupgrade(199444, 14, 9344),	-- Coif of Raging Tempests
+					iupgrade(199444, 14, 9344, CatalystableItem.Item),	-- Coif of Raging Tempests
 				}),
 				i(203620, {	-- Primalist Mail Leggings
-					iupgrade(199445, 14, 9344),	-- Greaves of Raging Tempests
+					iupgrade(199445, 14, 9344, CatalystableItem.Item),	-- Greaves of Raging Tempests
 				}),
 				i(203628, {	-- Primalist Mail Spaulders
-					iupgrade(199446, 14, 9344),	-- Shoulderguards of Raging Tempests
+					iupgrade(199446, 14, 9344, CatalystableItem.Item),	-- Shoulderguards of Raging Tempests
 				}),
 			}),
 			filter(PLATE, {
 				i(203634, {	-- Primalist Plate Belt
-					iupgrade(199439, 14, 9344),	-- Girdle of Raging Tempests
+					iupgrade(199439, 14, 9344, CatalystableItem.Item),	-- Girdle of Raging Tempests
 				}),
 				i(203640, {	-- Primalist Plate Boots
-					iupgrade(199435, 14, 9344),	-- Sabatons of Raging Tempests
+					iupgrade(199435, 14, 9344, CatalystableItem.Item),	-- Sabatons of Raging Tempests
 				}),
 				i(203633, {	-- Primalist Plate Bracers
-					iupgrade(199440, 14, 9344),	-- Armplates of Raging Tempests
+					iupgrade(199440, 14, 9344, CatalystableItem.Item),	-- Armplates of Raging Tempests
 				}),
 				i(203615, {	-- Primalist Plate Chestpiece
-					iupgrade(199434, 14, 9344),	-- Breastplate of Raging Tempests
+					iupgrade(199434, 14, 9344, CatalystableItem.Item),	-- Breastplate of Raging Tempests
 				}),
 				i(203643, {	-- Primalist Plate Gloves
-					iupgrade(199436, 14, 9344),	-- Gauntlets of Raging Tempests
+					iupgrade(199436, 14, 9344, CatalystableItem.Item),	-- Gauntlets of Raging Tempests
 				}),
 				i(203611, {	-- Primalist Plate Helm
-					iupgrade(199433, 14, 9344),	-- Helm of Raging Tempests
+					iupgrade(199433, 14, 9344, CatalystableItem.Item),	-- Helm of Raging Tempests
 				}),
 				i(203623, {	-- Primalist Plate Leggings
-					iupgrade(199437, 14, 9344),	-- Legguards of Raging Tempests
+					iupgrade(199437, 14, 9344, CatalystableItem.Item),	-- Legguards of Raging Tempests
 				}),
 				i(203626, {	-- Primalist Plate Spaulders
-					iupgrade(199438, 14, 9344),	-- Mantle of Raging Tempests
+					iupgrade(199438, 14, 9344, CatalystableItem.Item),	-- Mantle of Raging Tempests
 				}),
 			}),
 			n(WEAPONS, {
