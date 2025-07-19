e6f01593ad36ebd0696224cf14ee68330dcd1e51
Crieve <dylanfortune@live.com>
2025-07-18 18:33:26 -0700

    Fixed the COMPLETE_ICON, this one is also just supposed to be an icon.

--- .contrib/Parser/lib/Strings/classic/ATT-Classic/COMPLETE_ICON.lua
+++ .contrib/Parser/lib/Strings/classic/ATT-Classic/COMPLETE_ICON.lua
@@ -4,16 +4,6 @@ createLocalizationString({
 	icon = [[~_.asset("known_green")]],
 	export = true,
 	text = {
-		en = "Complete",
-		de = "Vollständig",
-		es = "Completado",
-		mx = "Completado",
-		fr = "Complet",
-		-- TODO: it = "",
-		-- TODO: pt = "",
-		ru = "Выполнено",
-		-- TODO: ko = "",
-		cn = "已完成",
-		-- TODO: tw = "",
+		en = "",
 	},
 })
