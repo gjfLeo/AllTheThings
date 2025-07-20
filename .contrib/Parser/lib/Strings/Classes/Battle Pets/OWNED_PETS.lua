f90763cb03093e6236f79fb9573118a21639a022
Crieve <dylanfortune@live.com>
2025-07-19 17:13:35 -0700

    Added a placeholder OWNED_PETS localization string.

---
+++ .contrib/Parser/lib/Strings/Classes/Battle Pets/OWNED_PETS.lua
@@ -0,0 +1,18 @@
+createLocalizationString({
+	readable = "Owned Pets:",
+	constant = "OWNED_PETS",
+	export = true,
+	text = {
+		en = "Owned Pets:",
+		-- TODO: de = "",
+		-- TODO: es = "",
+		-- TODO: mx = "",
+		-- TODO: fr = "",
+		-- TODO: it = "",
+		-- TODO: pt = "",
+		-- TODO: ru = "",
+		-- TODO: ko = "",
+		-- TODO: cn = "",
+		-- TODO: tw = "",
+	},
+})
