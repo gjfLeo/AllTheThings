1b77eb71b13b5fcfad739452799f086603e0471a
ImUnicke <ImUnicke+github@gmail.com>
2025-07-19 20:14:45 -0600

    [DB] Auchindoun converted to use InstanceHelper to give a standard structure example for WoD dungeons [WIP]

--- .contrib/Parser/DATAS/01 - Dungeons Raids/06 - Warlords of Draenor/Auchindoun.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/06 - Warlords of Draenor/Auchindoun.lua
@@ -1,185 +1,129 @@
 -----------------------------------------------------
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
+
+------ Encounter Constants ------
+local VIGILANT = 1185
+local SOULBINDER = 1186
+local AZZAKEL = 1216
+local TERONGOR = 1225
+
+------ EncounterToCRS ------
+local EncounterToCRS = {
+	[VIGILANT] = { 75839 },	-- Oregorger
+	[SOULBINDER] = { 76177 },	-- Gruul
+	[AZZAKEL] = { 75927 },	-- The Blast Furnace
+	[TERONGOR] = { 77734 },	-- Teron'gor
+}
+
+------ EncounterToLoot ------
+local EncounterToLoot = {
+	[VIGILANT] = {
+		i(110045),	-- Kamui's Crystalline Staff of Wizardry
+	},
+	[SOULBINDER] = {
+		i(110046),	-- Hammer of the Soulbinder
+		i(110047),	-- Soulcutter Mageblade
+	},
+	[AZZAKEL] = {
+		i(110048),	-- Azzakel's Boltslinger
+		i(109995),	-- Blood Seal of Azzakel
+	},
+	[TERONGOR] = {
+		i(110049),	-- Bloodblade of Teron'Gor
+		i(110050),	-- Dagger of the Sanguine Emeralds
+		i(110005),	-- Crystalline Blood Drop
+		i(110010),	-- Mote of Corruption
+	},
+}
+
+------ Zone Drops ----------
+local ZoneDropLoot = {
+}
+
+local InstanceHelper = CreateInstanceHelper(EncounterToCRS, EncounterToLoot, ZoneDropLoot)
+local Boss, BossOnly, Difficulty, BossWithHeader =
+InstanceHelper.Boss, InstanceHelper.BossOnly, InstanceHelper.Difficulty, InstanceHelper.BossWithHeader
+
 root(ROOTS.Instances, expansion(EXPANSION.WOD, bubbleDown({ ["timeline"] = { ADDED_6_0_3_LAUNCH } }, {
 	inst(547, {	-- Auchindoun
 		["lvl"] = 94,
 		["mapID"] = 593,
 		["coord"] = { 46.27, 73.92, TALADOR },
 		["groups"] = {
-			d(DIFFICULTY.DUNGEON.MULTI.NORMAL_PLUS, {
-				e(1185, {	-- Vigilant Kaathar
-					["crs"] = { 75839 },
-				}),
-				e(1186, {	-- Soulbinder Nyami
-					["crs"] = { 76177 },
-				}),
-				e(1216, {	-- Azzakel
-					["crs"] = { 75927 },
-				}),
-				e(1225, {	-- Teron'gor
-					["crs"] = { 77734 },
-					["g"] = {
-						ach(9039),	-- Auchindoun
-					},
+			Difficulty(DIFFICULTY.DUNGEON.MULTI.NORMAL_PLUS).AddGroups({
+				BossOnly(VIGILANT),
+				BossOnly(SOULBINDER),
+				BossOnly(AZZAKEL),
+				BossOnly(TERONGOR, {
+					ach(9039),	-- Auchindoun
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.MULTI.NORMAL_MYTHIC, {
-				e(1185, {	-- Vigilant Kaathar
-					["crs"] = { 75839 },
+			Difficulty(DIFFICULTY.DUNGEON.MULTI.NORMAL_MYTHIC).AddGroups({
+				Boss(VIGILANT, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL_MYTHIC, WAIST, FINGER),
-					["g"] = {
-						i(110045),	-- Kamui's Crystalline Staff of Wizardry
-					},
 				}),
-				e(1186, {	-- Soulbinder Nyami
-					["crs"] = { 76177 },
+				Boss(SOULBINDER, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL_MYTHIC, CHEST, NECK),
-					["g"] = {
-						i(110046),	-- Hammer of the Soulbinder
-						i(110047),	-- Soulcutter Mageblade
-					},
 				}),
-				e(1216, {	-- Azzakel
-					["crs"] = { 75927 },
+				Boss(AZZAKEL, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL_MYTHIC, WRIST, HANDS),
-					["g"] = {
-						i(110048),	-- Azzakel's Boltslinger
-						i(109995),	-- Blood Seal of Azzakel
-					},
 				}),
-				e(1225, {	-- Teron'gor
-					["crs"] = { 77734 },
+				Boss(TERONGOR, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL_MYTHIC, BACK, LEGS, FEET),
-					["g"] = {
-						i(110049),	-- Bloodblade of Teron'Gor
-						i(110050),	-- Dagger of the Sanguine Emeralds
-						i(110005),	-- Crystalline Blood Drop
-						i(110010),	-- Mote of Corruption
-					},
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.NORMAL, {
-				e(1185, {	-- Vigilant Kaathar
-					["crs"] = { 75839 },
+			Difficulty(DIFFICULTY.DUNGEON.NORMAL).AddGroups({
+				BossWithHeader(VIGILANT, HEADER_WARFORGED, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL, WAIST, FINGER),
-					["g"] = {
-						HEADER_WARFORGED({
-							i(110045),	-- Kamui's Crystalline Staff of Wizardry
-						}),
-					},
 				}),
-				e(1186, {	-- Soulbinder Nyami
-					["crs"] = { 76177 },
+				BossWithHeader(SOULBINDER, HEADER_WARFORGED, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL, CHEST, NECK),
-					["g"] = {
-						HEADER_WARFORGED({
-							i(110046),	-- Hammer of the Soulbinder
-							i(110047),	-- Soulcutter Mageblade
-						}),
-					},
 				}),
-				e(1216, {	-- Azzakel
-					["crs"] = { 75927 },
+				BossWithHeader(AZZAKEL, HEADER_WARFORGED, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL, WRIST, HANDS),
-					["g"] = {
-						HEADER_WARFORGED({
-							i(110048),	-- Azzakel's Boltslinger
-							i(109995),	-- Blood Seal of Azzakel
-						}),
-					},
 				}),
-				e(1225, {	-- Teron'gor
-					["crs"] = { 77734 },
+				BossWithHeader(TERONGOR, HEADER_WARFORGED, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.NORMAL, BACK, LEGS, FEET),
-					["g"] = {
-						HEADER_WARFORGED({
-							i(110049),	-- Bloodblade of Teron'Gor
-							i(110050),	-- Dagger of the Sanguine Emeralds
-							i(110005),	-- Crystalline Blood Drop
-							i(110010),	-- Mote of Corruption
-						}),
-					},
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.MULTI.HEROIC_PLUS, {
-				e(1185, {	-- Vigilant Kaathar
-					["crs"] = { 75839 },
+			Difficulty(DIFFICULTY.DUNGEON.MULTI.HEROIC_PLUS).AddGroups({
+				BossOnly(VIGILANT),
+				BossOnly(SOULBINDER, {
+					ach(9023, {	-- ...They All Fall Down
+						["crs"] = { 76283 },	-- Malefic Defender
+					}),
 				}),
-				e(1186, {	-- Soulbinder Nyami
-					["crs"] = { 76177 },
-					["g"] = {
-						ach(9023, {	-- ...They All Fall Down
-							["crs"] = { 76283 },	-- Malefic Defender
-						}),
-					},
+				BossOnly(AZZAKEL, {
+					ach(9551),	-- Demon's Souls
 				}),
-				e(1216, {	-- Azzakel
-					["crs"] = { 75927 },
-					["g"] = {
-						ach(9551),	-- Demon's Souls
-					},
-				}),
-				e(1225, {	-- Teron'gor
-					["crs"] = { 77734 },
-					["g"] = {
-						ach(9049),	-- Heroic: Auchindoun
-						ach(9371),	-- Heroic: Auchindoun Guild Run
-						ach(9552),	-- No Tag-backs!
-						un(REMOVED_FROM_GAME, i(114240)),	-- Corrupted Blood of Teron'gor
-					},
+				BossOnly(TERONGOR, {
+					ach(9049),	-- Heroic: Auchindoun
+					ach(9371),	-- Heroic: Auchindoun Guild Run
+					ach(9552),	-- No Tag-backs!
+					un(REMOVED_FROM_GAME, i(114240)),	-- Corrupted Blood of Teron'gor
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.HEROIC, {
-				e(1185, {	-- Vigilant Kaathar
-					["crs"] = { 75839 },
+			Difficulty(DIFFICULTY.DUNGEON.HEROIC).AddGroups({
+				Boss(VIGILANT, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.HEROIC, WAIST, FINGER),
-					["g"] = {
-						i(110045),	-- Kamui's Crystalline Staff of Wizardry
-					},
 				}),
-				e(1186, {	-- Soulbinder Nyami
-					["crs"] = { 76177 },
+				Boss(SOULBINDER, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.HEROIC, CHEST, NECK),
-					["g"] = {
-						i(110046),	-- Hammer of the Soulbinder
-						i(110047),	-- Soulcutter Mageblade
-					},
 				}),
-				e(1216, {	-- Azzakel
-					["crs"] = { 75927 },
+				Boss(AZZAKEL, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.HEROIC, WRIST, HANDS),
-					["g"] = {
-						i(110048),	-- Azzakel's Boltslinger
-						i(109995),	-- Blood Seal of Azzakel
-					},
 				}),
-				e(1225, {	-- Teron'gor
-					["crs"] = { 77734 },
+				Boss(TERONGOR, {
 					["sym"] = SYM_WOD_COMMON_DUNGEON_SLOTS(DIFFICULTY.DUNGEON.HEROIC, BACK, LEGS, FEET),
-					["g"] = {
-						i(110049),	-- Bloodblade of Teron'Gor
-						i(110050),	-- Dagger of the Sanguine Emeralds
-						i(110005),	-- Crystalline Blood Drop
-						i(110010),	-- Mote of Corruption
-					},
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.MYTHIC, {
-				e(1185, {	-- Vigilant Kaathar
-					["crs"] = { 75839 },
-				}),
-				e(1186, {	-- Soulbinder Nyami
-					["crs"] = { 76177 },
-				}),
-				e(1216, {	-- Azzakel
-					["crs"] = { 75927 },
-				}),
-				e(1225, {	-- Teron'gor
-					["crs"] = { 77734 },
-					["g"] = {
-						ach(10080),	-- Mythic: Auchindoun
-					},
+			Difficulty(DIFFICULTY.DUNGEON.MYTHIC).AddGroups({
+				BossOnly(VIGILANT),
+				BossOnly(SOULBINDER),
+				BossOnly(AZZAKEL),
+				BossOnly(TERONGOR, {
+					ach(10080),	-- Mythic: Auchindoun
 				}),
 			}),
 		},
