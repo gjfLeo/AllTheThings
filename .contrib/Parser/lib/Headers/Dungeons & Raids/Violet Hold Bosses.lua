8152d0dfd2ec164243374d79b9b0e651e213bca6
Goldenshacal <goldenshacal@gmail.com>
2025-07-22 02:31:01 +0200

    violet hold bosses are now grouped

---
+++ .contrib/Parser/lib/Headers/Dungeons & Raids/Violet Hold Bosses.lua
@@ -0,0 +1,21 @@
+VH_WAVE_6_BOSS = createHeader({
+	readable = "Wave 6 Boss",
+	icon = 135442,
+	text = {
+		en = "Wave 6 Boss",
+	},
+});
+VH_WAVE_12_BOSS = createHeader({
+	readable = "Wave 12 Boss",
+	icon = 236271,
+	text = {
+		en = "Wave 12 Boss",
+	},
+});
+VH_FINAL_WAVE_BOSS = createHeader({
+	readable = "Final Wave Boss",
+	icon = 135771,
+	text = {
+		en = "Final Wave Boss",
+	},
+});
