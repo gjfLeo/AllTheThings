8623fac0e4989222574f11fae88f5dbf17713a1c
Eremeir <20992519+Eremeir@users.noreply.github.com>
2025-07-29 01:07:16 -0500

    Update The Forge of Souls with InstanceHelper

--- .contrib/Parser/DATAS/01 - Dungeons Raids/03 - Wrath of the Lich King/The Forge of Souls.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/03 - Wrath of the Lich King/The Forge of Souls.lua
@@ -1,6 +1,22 @@
 -----------------------------------------------------
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
+
+------ Encounter Constants ------
+local BRONJAHM = 615;
+local DEVOURER = 616;
+
+------ EncounterToCRS ------
+local EncounterToCRS = {
+	[BRONJAHM] = { 36497 },	-- Bronjahm
+	[DEVOURER] = { 36502 },	-- Devourer of Souls
+};
+
+------ Boss Functions ------
+local InstanceHelper = CreateInstanceHelper(EncounterToCRS)
+local BossOnly, Difficulty =
+InstanceHelper.BossOnly, InstanceHelper.Difficulty
+
 root(ROOTS.Instances, expansion(EXPANSION.WRATH, applyclassicphase(WRATH_PHASE_FOUR, bubbleDownSelf({ ["timeline"] = { ADDED_3_3_0 } }, {
 	inst(280, {	-- The Forge of Souls
 		["mapID"] = THE_FORGE_OF_SOULS,
@@ -10,10 +26,10 @@ root(ROOTS.Instances, expansion(EXPANSION.WRATH, applyclassicphase(WRATH_PHASE_F
 			n(ACHIEVEMENTS, {
 				ach(4516, {	-- The Forge of Souls
 					crit(13315, {	-- Bronjahm
-						["_encounter"] = { 615, DIFFICULTY.DUNGEON.NORMAL },
+						["_encounter"] = { 615, DIFFICULTY.DUNGEON.MULTI.NORMAL_HEROIC },
 					}),
 					crit(13316, {	-- Devourer of Souls
-						["_encounter"] = { 616, DIFFICULTY.DUNGEON.NORMAL },
+						["_encounter"] = { 616, DIFFICULTY.DUNGEON.MULTI.NORMAL_HEROIC },
 					}),
 				}),
 				ach(4519, {	-- Heroic: The Forge of Souls
@@ -86,174 +102,151 @@ root(ROOTS.Instances, expansion(EXPANSION.WRATH, applyclassicphase(WRATH_PHASE_F
 					["lvl"] = lvlsquish(78, 78, 25),
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.NORMAL, {
+			-- #if AFTER 7.3.5
+			Difficulty(DIFFICULTY.DUNGEON.NORMAL).AddGroups({
 				n(ZONE_DROPS, {
 					i(49852),	-- Coffin Nail
 					i(49854),	-- Mantle of Tattered Feathers
 					i(49855),	-- Plated Grips of Korth'azz
 					i(49853),	-- Titanium Links of Lore
 				}),
-				e(615, {	-- Bronjahm
-					["creatureID"] = 36497,	-- Bronjahm
-					["groups"] = {
-						i(49783),	-- Lucky Old Sun
-						-- #if AFTER 7.3.5
-						i(50191),	-- Nighttime (7.3.5 - Used to be only Heroic Mode)
-						i(50197),	-- Eyes of Bewilderment (7.3.5 - Used to be only Heroic Mode)
-						i(50196),	-- Love's Prisoner (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49785),	-- Bewildering Shoulderpads
-						-- #if AFTER 7.3.5
-						i(50193),	-- Very Fashionable Shoulders (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49786),	-- Robes of the Cheating Heart
-						i(49787),	-- Seven Stormy Mornings
-						i(49788),	-- Cold Sweat Grips
-						-- #if AFTER 7.3.5
-						i(50194),	-- Weeping Gauntlets (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49784),	-- Minister's Number One Legplates
-						-- #if AFTER 7.3.5
-						i(50316),	-- Papa's Brand New Bag (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(50317),	-- Papa's New Bag
-					},
+			}),
+			Difficulty(DIFFICULTY.DUNGEON.MULTI.NORMAL_HEROIC).AddGroups({
+				BossOnly(BRONJAHM, {
+					i(49785),	-- Bewildering Shoulderpads
+					i(49788),	-- Cold Sweat Grips
+					i(50197),	-- Eyes of Bewilderment
+					i(50196),	-- Love's Prisoner
+					i(49783),	-- Lucky Old Sun
+					i(49784),	-- Minister's Number One Legplates
+					i(50191),	-- Nighttime
+					i(50316),	-- Papa's Brand New Bag
+					i(50317),	-- Papa's New Bag
+					i(49786),	-- Robes of the Cheating Heart
+					i(49787),	-- Seven Stormy Mornings
+					i(50193),	-- Very Fashionable Shoulders
+					i(50194),	-- Weeping Gauntlets
 				}),
-				e(616, {	-- Devourer of Souls
-					["creatureID"] = 36502,	-- Devourer of Souls
-					["groups"] = {
-						i(49790),	-- Blood Boil Lancet
-						i(49793),	-- Tower of the Mouldering Corpse
-						-- #if AFTER 7.3.5
-						i(50203),	-- Blood Weeper (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49789),	-- Heartshiver
-						-- #if AFTER 7.3.5
-						i(50210),	-- Seethe (7.3.5 - Used to be only Heroic Mode)
-						i(50206),	-- Frayed Scoundrel's Cap (7.3.5 - Used to be only Heroic Mode)
-						i(50214),	-- Helm of the Spirit Shock (7.3.5 - Used to be only Heroic Mode)
-						i(50211),	-- Arcane Loops of Anger (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49799),	-- Coil of Missing Gems
-						-- #if AFTER 7.3.5
-						i(50208),	-- Pauldrons of the Souleater (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49792),	-- Accursed Crawling Cape
-						i(49791),	-- Lost Reliquary Chestguard
-						-- #if AFTER 7.3.5
-						i(50213),	-- Mord'rethar Robes (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49797),	-- Brace Guards of the Starless Night
-						-- #if AFTER 7.3.5
-						i(50209),	-- Essence of Suffering (7.3.5 - Used to be only Heroic Mode)
-						i(50212),	-- Essence of Desire (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49796),	-- Essence of Anger
-						i(49794),	-- Legplates of Frozen Granite
-						-- #if AFTER 7.3.5
-						i(50207),	-- Black Spire Sabatons (7.3.5 - Used to be only Heroic Mode)
-						i(50215),	-- Recovered Reliquary Boots (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-						i(49795),	-- Sollerets of Suffering
-						i(49798),	-- Soul Screaming Boots
-						i(49800),	-- Spiteful Signet
-						-- #if AFTER 7.3.5
-						i(50198),	-- Needle-Encrusted Scorpion (7.3.5 - Used to be only Heroic Mode)
-						-- #endif
-					},
+				BossOnly(DEVOURER, {
+					i(49792),	-- Accursed Crawling Cape
+					i(50211),	-- Arcane Loops of Anger
+					i(50207),	-- Black Spire Sabatons
+					i(49790),	-- Blood Boil Lancet
+					i(50203),	-- Blood Weeper
+					i(49797),	-- Brace Guards of the Starless Night
+					i(49799),	-- Coil of Missing Gems
+					i(49796),	-- Essence of Anger
+					i(50212),	-- Essence of Desire
+					i(50209),	-- Essence of Suffering
+					i(50206),	-- Frayed Scoundrel's Cap
+					i(49789),	-- Heartshiver
+					i(50214),	-- Helm of the Spirit Shock
+					i(49794),	-- Legplates of Frozen Granite
+					i(49791),	-- Lost Reliquary Chestguard
+					i(50213),	-- Mord'rethar Robes
+					i(50198),	-- Needle-Encrusted Scorpion
+					i(50208),	-- Pauldrons of the Souleater
+					i(50215),	-- Recovered Reliquary Boots
+					i(50210),	-- Seethe
+					i(49795),	-- Sollerets of Suffering
+					i(49798),	-- Soul Screaming Boots
+					i(49800),	-- Spiteful Signet
+					i(49793),	-- Tower of the Mouldering Corpse
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.HEROIC, {
-				["lvl"] = lvlsquish(80, 80, 30),
-				["groups"] = {
-					n(ZONE_DROPS, {
-						a(i(50379)),	-- Battered Hilt [A]
-						h(i(50380)),	-- Battered Hilt [H]
-						i(50318),	-- Ghostly Wristwraps
-						i(50315),	-- Seven-Fingered Claws
-						i(50319),	-- Unsharpened Ice Razor
+			Difficulty(DIFFICULTY.DUNGEON.HEROIC, { ["lvl"] = lvlsquish(80, 80, 30) }).AddGroups({
+				n(ZONE_DROPS, {
+					a(i(50379)),	-- Battered Hilt [A]
+					h(i(50380)),	-- Battered Hilt [H]
+					i(50318),	-- Ghostly Wristwraps
+					i(50315),	-- Seven-Fingered Claws
+					i(50319),	-- Unsharpened Ice Razor
+				}),
+				BossOnly(BRONJAHM, {
+					i(50169, {	-- Papa's Brand New Knife
+						["timeline"] = { REMOVED_5_0_4 },
 					}),
-					e(615, {	-- Bronjahm
-						["creatureID"] = 36497,	-- Bronjahm
-						["groups"] = {
-							ach(4522),	-- Soul Power
-							-- #if AFTER 7.3.5
-							i(49783),	-- Lucky Old Sun (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50191),	-- Nighttime
-							i(50197),	-- Eyes of Bewilderment
-							i(50196),	-- Love's Prisoner
-							-- #if AFTER 7.3.5
-							i(49785),	-- Bewildering Shoulderpads (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50193),	-- Very Fashionable Shoulders
-							-- #if AFTER 7.3.5
-							i(49786),	-- Robes of the Cheating Heart (7.3.5 - Used to be only Normal Mode)
-							i(49787),	-- Seven Stormy Mornings (7.3.5 - Used to be only Normal Mode)
-							i(49788),	-- Cold Sweat Grips (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50194),	-- Weeping Gauntlets
-							-- #if AFTER 7.3.5
-							i(49784),	-- Minister's Number One Legplates (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50169, {	-- Papa's Brand New Knife
-								["timeline"] = { REMOVED_5_0_4 },
-							}),
-							i(50316),	-- Papa's Brand New Bag
-							-- #if AFTER 7.3.5
-							i(50317),	-- Papa's New Bag (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-						},
+					ach(4522),	-- Soul Power
+				}),
+				BossOnly(DEVOURER, {
+					ach(5112, {	-- Heroic: The Forge of Souls Guild Run
+						["timeline"] = { ADDED_4_0_3 },
 					}),
-					e(616, {	-- Devourer of Souls
-						["creatureID"] = 36502,	-- Devourer of Souls
-						["groups"] = {
-							ach(5112, {	-- Heroic: The Forge of Souls Guild Run
-								["timeline"] = { ADDED_4_0_3 },
-							}),
-							ach(4523),	-- Three Faced
-							-- #if AFTER 7.3.5
-							i(49790),	-- Blood Boil Lancet (7.3.5 - Used to be only Normal Mode)
-							i(49793),	-- Tower of the Mouldering Corpse (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50203),	-- Blood Weeper
-							-- #if AFTER 7.3.5
-							i(49789),	-- Heartshiver (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50210),	-- Seethe
-							i(50206),	-- Frayed Scoundrel's Cap
-							i(50214),	-- Helm of the Spirit Shock
-							i(50211),	-- Arcane Loops of Anger
-							-- #if AFTER 7.3.5
-							i(49799),	-- Coil of Missing Gems (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50208),	-- Pauldrons of the Souleater
-							-- #if AFTER 7.3.5
-							i(49792),	-- Accursed Crawling Cape (7.3.5 - Used to be only Normal Mode)
-							i(49791),	-- Lost Reliquary Chestguard (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50213),	-- Mord'rethar Robes
-							-- #if AFTER 7.3.5
-							i(49797),	-- Brace Guards of the Starless Night (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50209),	-- Essence of Suffering
-							i(50212),	-- Essence of Desire
-							-- #if AFTER 7.3.5
-							i(49796),	-- Essence of Anger (7.3.5 - Used to be only Normal Mode)
-							i(49794),	-- Legplates of Frozen Granite (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50207),	-- Black Spire Sabatons
-							i(50215),	-- Recovered Reliquary Boots
-							-- #if AFTER 7.3.5
-							i(49795),	-- Sollerets of Suffering (7.3.5 - Used to be only Normal Mode)
-							i(49798),	-- Soul Screaming Boots (7.3.5 - Used to be only Normal Mode)
-							i(49800),	-- Spiteful Signet (7.3.5 - Used to be only Normal Mode)
-							-- #endif
-							i(50198),	-- Needle-Encrusted Scorpion
-						},
-					}),
-				},
+					ach(4523),	-- Three Faced
+				}),
 			}),
+			-- #else
+			Difficulty(DIFFICULTY.DUNGEON.NORMAL).AddGroups({
+				n(ZONE_DROPS, {
+					i(49852),	-- Coffin Nail
+					i(49854),	-- Mantle of Tattered Feathers
+					i(49855),	-- Plated Grips of Korth'azz
+					i(49853),	-- Titanium Links of Lore
+				}),
+				BossOnly(BRONJAHM, {
+					i(49785),	-- Bewildering Shoulderpads
+					i(49788),	-- Cold Sweat Grips
+					i(49783),	-- Lucky Old Sun
+					i(49784),	-- Minister's Number One Legplates
+					i(50317),	-- Papa's New Bag
+					i(49786),	-- Robes of the Cheating Heart
+					i(49787),	-- Seven Stormy Mornings
+				}),
+				BossOnly(DEVOURER, {
+					i(49792),	-- Accursed Crawling Cape
+					i(49790),	-- Blood Boil Lancet
+					i(49797),	-- Brace Guards of the Starless Night
+					i(49799),	-- Coil of Missing Gems
+					i(49796),	-- Essence of Anger
+					i(49789),	-- Heartshiver
+					i(49794),	-- Legplates of Frozen Granite
+					i(49791),	-- Lost Reliquary Chestguard
+					i(49795),	-- Sollerets of Suffering
+					i(49798),	-- Soul Screaming Boots
+					i(49800),	-- Spiteful Signet
+					i(49793),	-- Tower of the Mouldering Corpse
+				}),
+			}),
+			Difficulty(DIFFICULTY.DUNGEON.HEROIC, { ["lvl"] = lvlsquish(80, 80, 30) }).AddGroups({
+				n(ZONE_DROPS, {
+					a(i(50379)),	-- Battered Hilt [A]
+					h(i(50380)),	-- Battered Hilt [H]
+					i(50318),	-- Ghostly Wristwraps
+					i(50315),	-- Seven-Fingered Claws
+					i(50319),	-- Unsharpened Ice Razor
+				}),
+				BossOnly(BRONJAHM, {
+					ach(4522),	-- Soul Power
+					i(50197),	-- Eyes of Bewilderment
+					i(50196),	-- Love's Prisoner
+					i(50191),	-- Nighttime
+					i(50169, {	-- Papa's Brand New Knife
+						["timeline"] = { REMOVED_5_0_4 },
+					}),
+					i(50316),	-- Papa's Brand New Bag
+					i(50193),	-- Very Fashionable Shoulders
+					i(50194),	-- Weeping Gauntlets
+				}),
+				BossOnly(DEVOURER, {
+					ach(5112, {	-- Heroic: The Forge of Souls Guild Run
+						["timeline"] = { ADDED_4_0_3 },
+					}),
+					ach(4523),	-- Three Faced
+					i(50211),	-- Arcane Loops of Anger
+					i(50207),	-- Black Spire Sabatons
+					i(50203),	-- Blood Weeper
+					i(50212),	-- Essence of Desire
+					i(50209),	-- Essence of Suffering
+					i(50206),	-- Frayed Scoundrel's Cap
+					i(50214),	-- Helm of the Spirit Shock
+					i(50213),	-- Mord'rethar Robes
+					i(50198),	-- Needle-Encrusted Scorpion
+					i(50208),	-- Pauldrons of the Souleater
+					i(50215),	-- Recovered Reliquary Boots
+					i(50210),	-- Seethe
+				}),
+			}),
+			-- #endif
 		},
 	}),
 }))));
