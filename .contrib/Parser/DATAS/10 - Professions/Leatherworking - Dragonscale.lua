0bc6e75d99e57029a5145e2a7ff846eaa046b7ab
Wouter <lostitz0r@gmail.com>
2025-08-01 13:02:58 +0200

    [DB] Classic: put Dragonscale/Ele/Tribal LW headers under Classic header

--- .contrib/Parser/DATAS/10 - Professions/Leatherworking - Dragonscale.lua
+++ .contrib/Parser/DATAS/10 - Professions/Leatherworking - Dragonscale.lua
@@ -1,260 +1,262 @@
 -- #if ANYCLASSIC
 profession(LEATHERWORKING, {
-	prof(10656, {	-- Dragonscale Leatherworking
-		["description"] = "These items can only be crafted by Leatherworkers who have completed the associated quest.\n\nNOTE: You may only have one of these specializations active per character. If you wish to finish your collection, you must level several Leatherworkers and complete the opposing specialization(s).",
-		["sourceQuests"] = {
-			5141,	-- Dragonscale Leatherworking [Alliance]
-			5145,	-- Dragonscale Leatherworking [Horde]
-		},
-		["timeline"] = { REMOVED_4_0_1 },
-		["groups"] = {
-			-- #if AFTER TBC
-			{
-				["name"] = "Chest",
-				["categoryID"] = 932,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Ebon Netherscale Breastplate",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35575,
-						["requireSkill"] = 10656,
-					}),
-					-- #if BEFORE CATA
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Golden Dragonstrike Breastplate",
-						["recipeID"] = 36079,
-						["requireSkill"] = 10656,
-					}),
-					-- #endif
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Netherstrike Breastplate",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35580,
-						["requireSkill"] = 10656,
-					}),
-				}
+	expansion(EXPANSION.CLASSIC, {
+		prof(10656, {	-- Dragonscale Leatherworking
+			["description"] = "These items can only be crafted by Leatherworkers who have completed the associated quest.\n\nNOTE: You may only have one of these specializations active per character. If you wish to finish your collection, you must level several Leatherworkers and complete the opposing specialization(s).",
+			["sourceQuests"] = {
+				5141,	-- Dragonscale Leatherworking [Alliance]
+				5145,	-- Dragonscale Leatherworking [Horde]
 			},
-			{
-				["name"] = "Bracers",
-				["categoryID"] = 933,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Ebon Netherscale Bracers",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35577,
-						["requireSkill"] = 10656,
-					}),
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Netherstrike Bracers",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35584,
-						["requireSkill"] = 10656,
-					}),
-				}
-			},
-			{
-				["name"] = "Belts",
-				["categoryID"] = 935,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Ebon Netherscale Belt",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35576,
-						["requireSkill"] = 10656,
-					}),
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Netherstrike Belt",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35582,
-						["requireSkill"] = 10656,
-					}),
-				}
-			},
-			-- #if BEFORE CATA
-			{
-				["name"] = "Pants",
-				["categoryID"] = 936,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Dragonstrike Leggings",
-						["recipeID"] = 36076,
-						["requireSkill"] = 10656,
-					}),
+			["timeline"] = { REMOVED_4_0_1 },
+			["groups"] = {
+				-- #if AFTER TBC
+				{
+					["name"] = "Chest",
+					["categoryID"] = 932,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Ebon Netherscale Breastplate",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35575,
+							["requireSkill"] = 10656,
+						}),
+						-- #if BEFORE CATA
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Golden Dragonstrike Breastplate",
+							["recipeID"] = 36079,
+							["requireSkill"] = 10656,
+						}),
+						-- #endif
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Netherstrike Breastplate",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35580,
+							["requireSkill"] = 10656,
+						}),
+					}
 				},
-			},
-			-- #endif
-			-- #else
-			{
-				["name"] = "Shoulders",
-				["categoryID"] = 252,
-				["groups"] = {
-					{
-						["name"] = "Black Dragonscale Shoulders",
-						["recipeID"] = 19094,
-						["requireSkill"] = 10656,
-						-- #if SEASON_OF_DISCOVERY
-						["timeline"] = { REMOVED_1_15_3 },
-						-- #endif
+				{
+					["name"] = "Bracers",
+					["categoryID"] = 933,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Ebon Netherscale Bracers",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35577,
+							["requireSkill"] = 10656,
+						}),
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Netherstrike Bracers",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35584,
+							["requireSkill"] = 10656,
+						}),
+					}
+				},
+				{
+					["name"] = "Belts",
+					["categoryID"] = 935,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Ebon Netherscale Belt",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35576,
+							["requireSkill"] = 10656,
+						}),
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Netherstrike Belt",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35582,
+							["requireSkill"] = 10656,
+						}),
+					}
+				},
+				-- #if BEFORE CATA
+				{
+					["name"] = "Pants",
+					["categoryID"] = 936,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Dragonstrike Leggings",
+							["recipeID"] = 36076,
+							["requireSkill"] = 10656,
+						}),
 					},
-					{
-						["name"] = "Blue Dragonscale Shoulders",
-						["timeline"] = { REMOVED_4_0_3, ADDED_10_1_7 },
-						["recipeID"] = 19089,
-						["requireSkill"] = 10656,
-					},
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Hardened Black Dragonscale Shoulders",
-						["recipeID"] = 461659,
-						["requireSkill"] = 10656,
-						["timeline"] = { ADDED_1_15_3 },
-					}),
-					-- #endif
-				}
-			},
-			{
-				["name"] = "Chest",
-				["categoryID"] = 253,
-				["groups"] = {
-					{
-						["name"] = "Black Dragonscale Breastplate",
-						["recipeID"] = 19085,
-						["requireSkill"] = 10656,
-						-- #if SEASON_OF_DISCOVERY
-						["timeline"] = { REMOVED_1_15_3 },
-						-- #endif
-					},
-					{
-						["name"] = "Blue Dragonscale Breastplate",
-						["timeline"] = { REMOVED_4_0_3 },
-						["recipeID"] = 19077,
-						["requireSkill"] = 10656,
-					},
-					{
-						["name"] = "Dragonscale Breastplate",
-						["recipeID"] = 10650,
-						["requireSkill"] = 10656,
-					},
-					applyclassicphase(PHASE_FOUR, {
-						["name"] = "Dreamscale Breastplate",
-						["recipeID"] = 24703,
-						["requireSkill"] = 10656,
-					}),
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Hardened Black Dragonscale Breastplate",
-						["recipeID"] = 461655,
-						["requireSkill"] = 10656,
-						["timeline"] = { ADDED_1_15_3 },
-					}),
-					-- #endif
-					{
-						["name"] = "Green Dragonscale Breastplate",
-						["recipeID"] = 19050,
-						["requireSkill"] = 10656,
-					},
-					{
-						["name"] = "Red Dragonscale Breastplate",
-						["timeline"] = { REMOVED_6_0_2 },
-						["recipeID"] = 19054,
-						["requireSkill"] = 10656,
-					},
-				}
-			},
-			{
-				["name"] = "Gloves",
-				["categoryID"] = 255,
-				["groups"] = {
-					applyclassicphase(PHASE_THREE_RECIPES, {
-						["name"] = "Chromatic Gauntlets",
-						["recipeID"] = 23708,
-						["requireSkill"] = 10656,
-					}),
-					{
-						["name"] = "Dragonscale Gauntlets",
-						["timeline"] = { REMOVED_4_0_3 },
-						["recipeID"] = 10619,
-						["requireSkill"] = 10656,
-					},
-					{
-						["name"] = "Green Dragonscale Gauntlets",
-						["recipeID"] = 24655,
-						["requireSkill"] = 10656,
-					},
-				}
-			},
-			{
-				["name"] = "Pants",
-				["categoryID"] = 257,
-				["groups"] = {
-					{
-						["name"] = "Black Dragonscale Leggings",
-						["recipeID"] = 19107,
-						["requireSkill"] = 10656,
-						-- #if SEASON_OF_DISCOVERY
-						["timeline"] = { REMOVED_1_15_3 },
-						-- #endif
-					},
-					{
-						["name"] = "Blue Dragonscale Leggings",
-						["recipeID"] = 24654,
-						["requireSkill"] = 10656,
-					},
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Hardened Black Dragonscale Leggings",
-						["recipeID"] = 461657,
-						["requireSkill"] = 10656,
-						["timeline"] = { ADDED_1_15_3 },
-					}),
-					-- #endif
-					{
-						["name"] = "Green Dragonscale Leggings",
-						["recipeID"] = 19060,
-						["requireSkill"] = 10656,
-					},
-				}
-			},
-			{
-				["name"] = "Boots",
-				["categoryID"] = 258,
-				["groups"] = {
-					{
-						["name"] = "Black Dragonscale Boots",
-						["recipeID"] = 20855,
-						["requireSkill"] = 10656,
-					},
-				}
-			},
-			{
-				["name"] = "Cloaks",
-				["categoryID"] = 259,
-				["groups"] = {
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Brilliant Chromatic Cloak",
-						["recipeID"] = 461653,
-						["requireSkill"] = 10656,
-						["timeline"] = { ADDED_1_15_3 },
-					}),
-					-- #endif
-					applyclassicphase(PHASE_ONE_DIREMAUL, {
-						["name"] = "Chromatic Cloak",
-						["recipeID"] = 22926,
-						["requireSkill"] = 10656,
-						["timeline"] = {
+				},
+				-- #endif
+				-- #else
+				{
+					["name"] = "Shoulders",
+					["categoryID"] = 252,
+					["groups"] = {
+						{
+							["name"] = "Black Dragonscale Shoulders",
+							["recipeID"] = 19094,
+							["requireSkill"] = 10656,
 							-- #if SEASON_OF_DISCOVERY
-							REMOVED_1_15_3,
-							-- #else
-							REMOVED_4_0_3,
-							ADDED_10_1_5,
+							["timeline"] = { REMOVED_1_15_3 },
 							-- #endif
 						},
-					}),
+						{
+							["name"] = "Blue Dragonscale Shoulders",
+							["timeline"] = { REMOVED_4_0_3, ADDED_10_1_7 },
+							["recipeID"] = 19089,
+							["requireSkill"] = 10656,
+						},
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Hardened Black Dragonscale Shoulders",
+							["recipeID"] = 461659,
+							["requireSkill"] = 10656,
+							["timeline"] = { ADDED_1_15_3 },
+						}),
+						-- #endif
+					}
 				},
+				{
+					["name"] = "Chest",
+					["categoryID"] = 253,
+					["groups"] = {
+						{
+							["name"] = "Black Dragonscale Breastplate",
+							["recipeID"] = 19085,
+							["requireSkill"] = 10656,
+							-- #if SEASON_OF_DISCOVERY
+							["timeline"] = { REMOVED_1_15_3 },
+							-- #endif
+						},
+						{
+							["name"] = "Blue Dragonscale Breastplate",
+							["timeline"] = { REMOVED_4_0_3 },
+							["recipeID"] = 19077,
+							["requireSkill"] = 10656,
+						},
+						{
+							["name"] = "Dragonscale Breastplate",
+							["recipeID"] = 10650,
+							["requireSkill"] = 10656,
+						},
+						applyclassicphase(PHASE_FOUR, {
+							["name"] = "Dreamscale Breastplate",
+							["recipeID"] = 24703,
+							["requireSkill"] = 10656,
+						}),
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Hardened Black Dragonscale Breastplate",
+							["recipeID"] = 461655,
+							["requireSkill"] = 10656,
+							["timeline"] = { ADDED_1_15_3 },
+						}),
+						-- #endif
+						{
+							["name"] = "Green Dragonscale Breastplate",
+							["recipeID"] = 19050,
+							["requireSkill"] = 10656,
+						},
+						{
+							["name"] = "Red Dragonscale Breastplate",
+							["timeline"] = { REMOVED_6_0_2 },
+							["recipeID"] = 19054,
+							["requireSkill"] = 10656,
+						},
+					}
+				},
+				{
+					["name"] = "Gloves",
+					["categoryID"] = 255,
+					["groups"] = {
+						applyclassicphase(PHASE_THREE_RECIPES, {
+							["name"] = "Chromatic Gauntlets",
+							["recipeID"] = 23708,
+							["requireSkill"] = 10656,
+						}),
+						{
+							["name"] = "Dragonscale Gauntlets",
+							["timeline"] = { REMOVED_4_0_3 },
+							["recipeID"] = 10619,
+							["requireSkill"] = 10656,
+						},
+						{
+							["name"] = "Green Dragonscale Gauntlets",
+							["recipeID"] = 24655,
+							["requireSkill"] = 10656,
+						},
+					}
+				},
+				{
+					["name"] = "Pants",
+					["categoryID"] = 257,
+					["groups"] = {
+						{
+							["name"] = "Black Dragonscale Leggings",
+							["recipeID"] = 19107,
+							["requireSkill"] = 10656,
+							-- #if SEASON_OF_DISCOVERY
+							["timeline"] = { REMOVED_1_15_3 },
+							-- #endif
+						},
+						{
+							["name"] = "Blue Dragonscale Leggings",
+							["recipeID"] = 24654,
+							["requireSkill"] = 10656,
+						},
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Hardened Black Dragonscale Leggings",
+							["recipeID"] = 461657,
+							["requireSkill"] = 10656,
+							["timeline"] = { ADDED_1_15_3 },
+						}),
+						-- #endif
+						{
+							["name"] = "Green Dragonscale Leggings",
+							["recipeID"] = 19060,
+							["requireSkill"] = 10656,
+						},
+					}
+				},
+				{
+					["name"] = "Boots",
+					["categoryID"] = 258,
+					["groups"] = {
+						{
+							["name"] = "Black Dragonscale Boots",
+							["recipeID"] = 20855,
+							["requireSkill"] = 10656,
+						},
+					}
+				},
+				{
+					["name"] = "Cloaks",
+					["categoryID"] = 259,
+					["groups"] = {
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Brilliant Chromatic Cloak",
+							["recipeID"] = 461653,
+							["requireSkill"] = 10656,
+							["timeline"] = { ADDED_1_15_3 },
+						}),
+						-- #endif
+						applyclassicphase(PHASE_ONE_DIREMAUL, {
+							["name"] = "Chromatic Cloak",
+							["recipeID"] = 22926,
+							["requireSkill"] = 10656,
+							["timeline"] = {
+								-- #if SEASON_OF_DISCOVERY
+								REMOVED_1_15_3,
+								-- #else
+								REMOVED_4_0_3,
+								ADDED_10_1_5,
+								-- #endif
+							},
+						}),
+					},
+				},
+				-- #endif
 			},
-			-- #endif
-		},
-	}),
+		}),
+	})
 });
 -- #endif
--- .contrib/Parser/DATAS/10 - Professions/Leatherworking - Elemental.lua
+++ .contrib/Parser/DATAS/10 - Professions/Leatherworking - Elemental.lua
@@ -1,247 +1,249 @@
 -- #if ANYCLASSIC
 profession(LEATHERWORKING, {
-	prof(10658, {	-- Elemental Leatherworking
-		["description"] = "These items can only be crafted by Leatherworkers who have completed the associated quest.\n\nNOTE: You may only have one of these specializations active per character. If you wish to finish your collection, you must level several Leatherworkers and complete the opposing specialization(s).",
-		["sourceQuests"] = {
-			5144,	-- Elemental Leatherworking [Alliance]
-			5146,	-- Elemental Leatherworking [Horde]
-		},
-		["timeline"] = { REMOVED_4_0_1 },
-		["groups"] = {
-			-- #if AFTER TBC
-			{
-				["name"] = "Chest",
-				["categoryID"] = 932,
-				["groups"] = {
-					-- #if BEFORE CATA
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Primalstorm Breastplate",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 36077,
-						["requireSkill"] = 10658,
-					}),
-					-- #endif
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Primalstrike Vest",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35589,
-						["requireSkill"] = 10658,
-					}),
-				}
+	expansion(EXPANSION.CLASSIC, {
+		prof(10658, {	-- Elemental Leatherworking
+			["description"] = "These items can only be crafted by Leatherworkers who have completed the associated quest.\n\nNOTE: You may only have one of these specializations active per character. If you wish to finish your collection, you must level several Leatherworkers and complete the opposing specialization(s).",
+			["sourceQuests"] = {
+				5144,	-- Elemental Leatherworking [Alliance]
+				5146,	-- Elemental Leatherworking [Horde]
 			},
-			{
-				["name"] = "Bracers",
-				["categoryID"] = 933,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Primalstrike Bracers",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35591,
-						["requireSkill"] = 10658,
-					}),
-				}
-			},
-			{
-				["name"] = "Belts",
-				["categoryID"] = 935,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Primalstrike Belt",
-						["timeline"] = { REMOVED_4_0_1 },
-						["recipeID"] = 35590,
-						["requireSkill"] = 10658,
-					}),
-				}
-			},
-			-- #if BEFORE CATA
-			{
-				["name"] = "Pants",
-				["categoryID"] = 936,
-				["groups"] = {
-					applyclassicphase(TBC_PHASE_ONE, {
-						["name"] = "Blackstorm Leggings",
-						["recipeID"] = 36074,
-						["requireSkill"] = 10658,
-					}),
-				},
-			},
-			-- #endif
-			-- #else
-			{
-				["name"] = "Helms",
-				["categoryID"] = 251,
-				["groups"] = {
-					{
-						["name"] = "Helm of Fire",
-						["recipeID"] = 10632,
-						["requireSkill"] = 10658,
-					},
-					{
-						["name"] = "Molten Helm",
-						["recipeID"] = 20854,
-						["requireSkill"] = 10658,
-					},
-				},
-			},
-			{
-				["name"] = "Shoulders",
-				["categoryID"] = 252,
-				["groups"] = {
-					{
-						["name"] = "Living Shoulders",
-						["recipeID"] = 19061,
-						["requireSkill"] = 10658,
-					},
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Masterwork Volcanic Shoulders",
-						["recipeID"] = 461663,
-						["requireSkill"] = 10658,
-					}),
-					-- #endif
-					{
-						["name"] = "Stormshroud Shoulders",
-						["timeline"] = { ADDED_1_11_1, REMOVED_4_0_3 },
-						["recipeID"] = 19090,
-						["requireSkill"] = 10658,
-					},
-					{
-						["name"] = "Volcanic Shoulders",
-						["recipeID"] = 19101,
-						["requireSkill"] = 10658,
-						-- #if SEASON_OF_DISCOVERY
-						["timeline"] = { REMOVED_1_15_3 },
+			["timeline"] = { REMOVED_4_0_1 },
+			["groups"] = {
+				-- #if AFTER TBC
+				{
+					["name"] = "Chest",
+					["categoryID"] = 932,
+					["groups"] = {
+						-- #if BEFORE CATA
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Primalstorm Breastplate",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 36077,
+							["requireSkill"] = 10658,
+						}),
 						-- #endif
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Primalstrike Vest",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35589,
+							["requireSkill"] = 10658,
+						}),
+					}
+				},
+				{
+					["name"] = "Bracers",
+					["categoryID"] = 933,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Primalstrike Bracers",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35591,
+							["requireSkill"] = 10658,
+						}),
+					}
+				},
+				{
+					["name"] = "Belts",
+					["categoryID"] = 935,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Primalstrike Belt",
+							["timeline"] = { REMOVED_4_0_1 },
+							["recipeID"] = 35590,
+							["requireSkill"] = 10658,
+						}),
+					}
+				},
+				-- #if BEFORE CATA
+				{
+					["name"] = "Pants",
+					["categoryID"] = 936,
+					["groups"] = {
+						applyclassicphase(TBC_PHASE_ONE, {
+							["name"] = "Blackstorm Leggings",
+							["recipeID"] = 36074,
+							["requireSkill"] = 10658,
+						}),
 					},
-				}
-			},
-			{
-				["name"] = "Chest",
-				["categoryID"] = 253,
-				["groups"] = {
-					{
-						["name"] = "Living Breastplate",
-						["timeline"] = { REMOVED_4_0_3, ADDED_10_1_7 },
-						["recipeID"] = 19095,
-						["requireSkill"] = 10658,
+				},
+				-- #endif
+				-- #else
+				{
+					["name"] = "Helms",
+					["categoryID"] = 251,
+					["groups"] = {
+						{
+							["name"] = "Helm of Fire",
+							["recipeID"] = 10632,
+							["requireSkill"] = 10658,
+						},
+						{
+							["name"] = "Molten Helm",
+							["recipeID"] = 20854,
+							["requireSkill"] = 10658,
+						},
 					},
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Masterwork Volcanic Breastplate",
-						["timeline"] = { ADDED_1_15_3 },
-						["recipeID"] = 461661,
-						["requireSkill"] = 10658,
-					}),
-					-- #endif
-					{
-						["name"] = "Stormshroud Armor",
-						["timeline"] = { ADDED_1_11_1, REMOVED_4_0_3 },
-						["recipeID"] = 19079,
-						["requireSkill"] = 10658,
-					},
-					{
-						["name"] = "Volcanic Breastplate",
-						["recipeID"] = 19076,
-						["requireSkill"] = 10658,
+				},
+				{
+					["name"] = "Shoulders",
+					["categoryID"] = 252,
+					["groups"] = {
+						{
+							["name"] = "Living Shoulders",
+							["recipeID"] = 19061,
+							["requireSkill"] = 10658,
+						},
 						-- #if SEASON_OF_DISCOVERY
-						["timeline"] = { REMOVED_1_15_3 },
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Masterwork Volcanic Shoulders",
+							["recipeID"] = 461663,
+							["requireSkill"] = 10658,
+						}),
 						-- #endif
-					},
-				}
-			},
-			{
-				["name"] = "Gloves",
-				["categoryID"] = 255,
-				["groups"] = {
-					{
-						["name"] = "Gauntlets of the Sea",
-						["recipeID"] = 10630,
-						["requireSkill"] = 10658,
-					},
-					{
-						["name"] = "Stormshroud Gloves",
-						["timeline"] = { ADDED_1_11_1, REMOVED_4_0_3 },
-						["recipeID"] = 26279,
-						["requireSkill"] = 10658,
-					},
-				},
-			},
-			{
-				["name"] = "Belts",
-				["categoryID"] = 256,
-				["groups"] = {
-					applyclassicphase(PHASE_THREE_RECIPES, {
-						["name"] = "Molten Belt",
-						["recipeID"] = 23710,
-						["requireSkill"] = 10658,
-					}),
-				},
-			},
-			{
-				["name"] = "Pants",
-				["categoryID"] = 257,
-				["groups"] = {
-					{
-						["name"] = "Living Leggings",
-						["timeline"] = { REMOVED_4_0_3, ADDED_10_1_7 },
-						["recipeID"] = 19078,
-						["requireSkill"] = 10658,
-					},
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Masterwork Volcanic Leggings",
-						["recipeID"] = 461665,
-						["requireSkill"] = 10658,
-						["timeline"] = { ADDED_1_15_3 },
-					}),
-					-- #endif
-					{
-						["name"] = "Stormshroud Pants",
-						["recipeID"] = 19067,
-						["requireSkill"] = 10658,
-					},
-					{
-						["name"] = "Volcanic Leggings",
-						["timeline"] = {
+						{
+							["name"] = "Stormshroud Shoulders",
+							["timeline"] = { ADDED_1_11_1, REMOVED_4_0_3 },
+							["recipeID"] = 19090,
+							["requireSkill"] = 10658,
+						},
+						{
+							["name"] = "Volcanic Shoulders",
+							["recipeID"] = 19101,
+							["requireSkill"] = 10658,
 							-- #if SEASON_OF_DISCOVERY
-							REMOVED_1_15_3,
-							-- #else
-							REMOVED_4_0_3, ADDED_10_1_7,
+							["timeline"] = { REMOVED_1_15_3 },
 							-- #endif
 						},
-						["recipeID"] = 19059,
-						["requireSkill"] = 10658,
-					},
-				}
-			},
-			{
-				["name"] = "Cloaks",
-				["categoryID"] = 259,
-				["groups"] = {
-					-- #if SEASON_OF_DISCOVERY
-					applyclassicphase(SOD_PHASE_FOUR, {
-						["name"] = "Mastercrafted Shifting Cloak",
-						["timeline"] = { ADDED_1_15_3 },
-						["recipeID"] = 461690,
-						["requireSkill"] = 10658,
-					}),
-					-- #endif
-					applyclassicphase(PHASE_ONE_DIREMAUL, {
-						["name"] = "Shifting Cloak",
-						["timeline"] = {
+					}
+				},
+				{
+					["name"] = "Chest",
+					["categoryID"] = 253,
+					["groups"] = {
+						{
+							["name"] = "Living Breastplate",
+							["timeline"] = { REMOVED_4_0_3, ADDED_10_1_7 },
+							["recipeID"] = 19095,
+							["requireSkill"] = 10658,
+						},
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Masterwork Volcanic Breastplate",
+							["timeline"] = { ADDED_1_15_3 },
+							["recipeID"] = 461661,
+							["requireSkill"] = 10658,
+						}),
+						-- #endif
+						{
+							["name"] = "Stormshroud Armor",
+							["timeline"] = { ADDED_1_11_1, REMOVED_4_0_3 },
+							["recipeID"] = 19079,
+							["requireSkill"] = 10658,
+						},
+						{
+							["name"] = "Volcanic Breastplate",
+							["recipeID"] = 19076,
+							["requireSkill"] = 10658,
 							-- #if SEASON_OF_DISCOVERY
-							REMOVED_1_15_3,
-							-- #else
-							REMOVED_4_0_3, ADDED_10_1_5,
+							["timeline"] = { REMOVED_1_15_3 },
 							-- #endif
 						},
-						["recipeID"] = 22928,
-						["requireSkill"] = 10658,
-					}),
-				}
+					}
+				},
+				{
+					["name"] = "Gloves",
+					["categoryID"] = 255,
+					["groups"] = {
+						{
+							["name"] = "Gauntlets of the Sea",
+							["recipeID"] = 10630,
+							["requireSkill"] = 10658,
+						},
+						{
+							["name"] = "Stormshroud Gloves",
+							["timeline"] = { ADDED_1_11_1, REMOVED_4_0_3 },
+							["recipeID"] = 26279,
+							["requireSkill"] = 10658,
+						},
+					},
+				},
+				{
+					["name"] = "Belts",
+					["categoryID"] = 256,
+					["groups"] = {
+						applyclassicphase(PHASE_THREE_RECIPES, {
+							["name"] = "Molten Belt",
+							["recipeID"] = 23710,
+							["requireSkill"] = 10658,
+						}),
+					},
+				},
+				{
+					["name"] = "Pants",
+					["categoryID"] = 257,
+					["groups"] = {
+						{
+							["name"] = "Living Leggings",
+							["timeline"] = { REMOVED_4_0_3, ADDED_10_1_7 },
+							["recipeID"] = 19078,
+							["requireSkill"] = 10658,
+						},
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Masterwork Volcanic Leggings",
+							["recipeID"] = 461665,
+							["requireSkill"] = 10658,
+							["timeline"] = { ADDED_1_15_3 },
+						}),
+						-- #endif
+						{
+							["name"] = "Stormshroud Pants",
+							["recipeID"] = 19067,
+							["requireSkill"] = 10658,
+						},
+						{
+							["name"] = "Volcanic Leggings",
+							["timeline"] = {
+								-- #if SEASON_OF_DISCOVERY
+								REMOVED_1_15_3,
+								-- #else
+								REMOVED_4_0_3, ADDED_10_1_7,
+								-- #endif
+							},
+							["recipeID"] = 19059,
+							["requireSkill"] = 10658,
+						},
+					}
+				},
+				{
+					["name"] = "Cloaks",
+					["categoryID"] = 259,
+					["groups"] = {
+						-- #if SEASON_OF_DISCOVERY
+						applyclassicphase(SOD_PHASE_FOUR, {
+							["name"] = "Mastercrafted Shifting Cloak",
+							["timeline"] = { ADDED_1_15_3 },
+							["recipeID"] = 461690,
+							["requireSkill"] = 10658,
+						}),
+						-- #endif
+						applyclassicphase(PHASE_ONE_DIREMAUL, {
+							["name"] = "Shifting Cloak",
+							["timeline"] = {
+								-- #if SEASON_OF_DISCOVERY
+								REMOVED_1_15_3,
+								-- #else
+								REMOVED_4_0_3, ADDED_10_1_5,
+								-- #endif
+							},
+							["recipeID"] = 22928,
+							["requireSkill"] = 10658,
+						}),
+					}
+				},
+				-- #endif
 			},
-			-- #endif
-		},
+		}),
 	}),
 });
 -- #endif
