83134545b47a84675631bfd61cca408adbbcb23b
Crieve <dylanfortune@live.com>
2025-08-03 21:58:02 -0700

    [Parser] Fixed a bug that was causing Greater Inscriptions (the shoulder enchants) to be treated as collectible heirlooms.

--- .contrib/Source Code/Parser/Framework/Framework.Processing.cs
+++ .contrib/Source Code/Parser/Framework/Framework.Processing.cs
@@ -4471,6 +4471,9 @@ namespace ATT
                         case Objects.Filters.Recipe:
                         case Objects.Filters.Mount:
                             return;
+                        case Objects.Filters.Consumable:
+                            if(!data.ContainsKey("factionID")) return;
+                            break;
                     }
 
                     //LogDebugFormatted("ItemID:{0} Marked as Heirloom. Filter: {1}", itemID, filter.ToString());
