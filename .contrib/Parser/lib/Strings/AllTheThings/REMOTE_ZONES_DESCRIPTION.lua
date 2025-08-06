4f877c4acc380c1bc87ceb4c2799dce219618c17
ImUnicke <ImUnicke+github@gmail.com>
2025-08-06 16:08:55 -0600

    [Logic] Retail: Adjusted minilist a bit * Content Sourced in another real Zone but shown in the current Zone will now be contained in a 'Remote Zones' header to help reduce some excessive header bloat in some main cities and Zones (i.e. Catalysts/Vendors from Raids, remote zone quests available in a city, etc.) [This does not affect content normally mapped into the minilist otherwise]

---
+++ .contrib/Parser/lib/Strings/AllTheThings/REMOTE_ZONES_DESCRIPTION.lua
@@ -0,0 +1,18 @@
+createLocalizationString({
+	readable = "Remote Zones",
+	constant = "REMOTE_ZONES_DESCRIPTION",
+	export = true,
+	text = {
+		en = "Contains content which is available in the current Zone, but is directly Sourced in another Zone.",
+		-- TODO: de = ""
+		-- TODO: es = ""
+		-- TODO: mx = ""
+		-- TODO: fr = ""
+		-- TODO: it = ""
+		-- TODO: pt = ""
+		-- TODO: ru = ""
+		-- TODO: ko = ""
+		-- TODO: cn = ""
+		-- TODO: tw = ""
+	},
+})
