a05efc835857c261401b5a26644bb44aba2779f0
ImUnicke <ImUnicke+github@gmail.com>
2025-08-06 16:45:54 -0600

    [DB] Don't need to 'sym' BattlePets which are Sourced with possible coord(s)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Battle Pets.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/1 - Dornogal/Battle Pets.lua
@@ -4,15 +4,9 @@
 root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, {
 	m(DORNOGAL, {
 		petbattle(filter(BATTLE_PETS, {
-			["sym"] = {{"select","speciesID",
-				4541,	-- Bedrock Stonecharger (PET!)
-			}},
-			["groups"] = {
-
-				pet(4518, {	-- Magmashell Crawler (PET!)
-					["coord"] = { 47.9, 56.0, DORNOGAL },
-				}),
-			},
+			pet(4518, {	-- Magmashell Crawler (PET!)
+				["coord"] = { 47.9, 56.0, DORNOGAL },
+			}),
 		})),
 	}),
 })));
