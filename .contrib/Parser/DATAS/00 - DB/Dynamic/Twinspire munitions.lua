40ac286082164c2f072feb2d6335d2d3c551b183
Crieve <dylanfortune@live.com>
2025-08-06 11:29:56 -0700

    Added object data for Twinspire munitions.

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/Twinspire munitions.lua
@@ -0,0 +1,9 @@
+local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[215195] = {
+		readable = "Twinspire munitions",
+		text = {
+			en = "Twinspire munitions",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
