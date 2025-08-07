f0091a1dcc1b7baca12f589cba0d5b9c455b0368
ImUnicke <ImUnicke+github@gmail.com>
2025-08-06 23:47:27 -0600

    [DB] Couple K'aresh rare updates

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/K'aresh/Rares.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/K'aresh/Rares.lua
@@ -55,6 +55,7 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_2_0 } }, 
 				["isDaily"] = IGNORED_VALUE,
 				["isWeekly"] = true,
 				["questID"] = 90698,
+				["provider"] = { "o", 517436 },	-- Vignette
 				["groups"] = { i(239469) },	-- Reshii Skirmisher's Pauldrons
 			}),
 			n(245998, {	-- Heka'tamos <the Elemental Disjunction>
@@ -154,6 +155,7 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_2_0 } }, 
 					}),
 					i(246240),	-- Devoured Energy-Pod
 					i(240112),	-- Reshii Scout's Blade
+					i(240120),	-- Reshii Magi's Lantern
 				},
 			}),
 			n(232111, {	-- The Nightreaver
